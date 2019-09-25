f510371eabc72e8bdbfb7bd33010fbfa08baab72
Pixxl <oliverhalenius@yahoo.se>
2019-09-25 20:48:55 +0200

    added UI to create new event window, and added the goals project

---
+++ Goals/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Goals
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("These are the goals and todos of the project!");
+			Console.WriteLine("");
+		}
+	}
+}
