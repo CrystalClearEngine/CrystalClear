6e70f90e104d7dd53f27efac2900bf494776cda5
Pixxl <oliverhalenius@yahoo.se>
2019-09-26 15:56:59 +0200

    worked on ScriptingUtils (now called ScriptUtils), much work on usersettings

--- Scripting/ScriptUtils.cs
+++ Scripting/ScriptUtils.cs
@@ -9,9 +9,25 @@ namespace Scripting
 {
 	public static class UserSettings
 	{
+		public static string savePath = Environment.CurrentDirectory + @"\settings";
+
 		public static void SaveSetting(Setting setting)
 		{
-			
+			string[] lines = File.ReadAllLines(savePath); //All settings in the file
+			for (int i = 0; i < lines.Length; i++)
+			{
+				string line = lines[i];
+				if (line.StartsWith(setting.name)) //This setting should be overwritten as it is the setting we want to change
+				{
+					File.WriteAllLines(savePath, lines.Where((v, j) => j != i));
+					File.AppendAllText(savePath, "\n" + SettingToString(setting));
+				}
+			}
+		}
+
+		internal static string SettingToString(Setting setting)
+		{
+			return setting.name + ": " + setting.value;
 		}
 
 		public struct Setting
