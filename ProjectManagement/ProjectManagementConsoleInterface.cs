2c81ea5b283e3eb010ec398af6fddccd16a57c2b
Pixxl <oliverhalenius@yahoo.se>
2019-09-29 12:44:21 +0200

    projectmanagement works now, no loading but creating and deleting projects works if a little hacki-ly

--- ProjectManagement/ProjectManagementConsoleInterface.cs
+++ ProjectManagement/ProjectManagementConsoleInterface.cs
@@ -35,9 +35,9 @@ namespace ProjectManagement
 			switch (inputArray[0])
 			{
 				case "new":
-					if (inputArray.Length >= 2)
-						NewProject(inputArray[1], inputArray[2]);
-					else
+					//if (inputArray.Length >= 2)
+					//	NewProject(inputArray[1], inputArray[2]);
+					//else
 						NewProject(inputArray[1]);
 					break;
 				case "use":
@@ -46,12 +46,94 @@ namespace ProjectManagement
 				case "del":
 					DeleteProject(inputArray[1]);
 					break;
+				case "lst":
+					//if (inputArray.Length >= 2)
+					//	NewProject(inputArray[1], inputArray[2]);
+					//else
+					ListProjects();
+					break;
 				case "in":
 					workInPath = inputArray[1];
 					break;
 				default:
 					break;
 			}
+		}
+
+		private static void ListProjects()
+		{
+			foreach (string folderPath in Directory.GetDirectories(workInPath))
+			{
+				if (IsProject(folderPath))
+				{
+					Console.WriteLine(GetProjectName(folderPath) + " - " + folderPath);
+				}
+			}
+		}
+
+		private static bool IsProject(string path)
+		{
+			foreach (var file in Directory.GetFiles(path))
+			{
+				if (file.EndsWith(".crystalcore"))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private static string GetProjectName(string path)
+		{
+			string crystalCorePath = null;
+			foreach (var file in Directory.GetFiles(path))
+			{
+				if (file.EndsWith(".crystalcore"))
+				{
+					crystalCorePath = file;
+				}
+			}
+
+			XmlSerializer xs = new XmlSerializer(typeof(ProjectInfo));
+			using (var sr = new StreamReader(crystalCorePath))
+			{
+				ProjectInfo projectInfo = (ProjectInfo)xs.Deserialize(sr);
+				return projectInfo.name;
+			}
+		}
+
+		private static void DeleteProject(string name, string path = null)
+		{
+			Console.WriteLine("Press 'y' to proceed deleting");
+			if (Console.ReadKey().KeyChar != 'y')
+			{
+				Console.WriteLine("Cancelled");
+				return;
+			}
+
+
+			string folderPath = workInPath + @"\" + name;
+			if (path != null)
+				folderPath = path + @"\" + name;
+
+			if (!IsProject(folderPath))
+			{
+				Console.WriteLine("The folder selected is not a project folder.");
+				return;
+			}
+
+			DirectoryInfo di = new DirectoryInfo(folderPath);
+
+			foreach (FileInfo file in di.GetFiles())
+			{
+				file.Delete();
+			}
+			foreach (DirectoryInfo dir in di.GetDirectories())
+			{
+				dir.Delete(true);
+			}
+
+			di.Delete();
 		}
 
 		static void NewProject(string name, string path = null)
