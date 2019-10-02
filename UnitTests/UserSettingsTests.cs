fe14b8a18345b8229ae2a6aa6af4294c64934082
Pixxl <oliverhalenius@yahoo.se>
2019-10-02 17:05:21 +0200

    Start working on tests

---
+++ UnitTests/UserSettingsTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+	public class UserSettingsTests
+	{
+		[TestClass]
+		public class CreateNewSettingTest
+		{
+			[TestMethod]
+			public void CreateNewSetting()
+			{
+				object ObjectToStore = "";
+			}
+		}
+	}
+}
