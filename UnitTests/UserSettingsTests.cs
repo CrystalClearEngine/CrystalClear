92b01ca15752483bb0ace04d12e62cc8250e376e
Pixxl <oliverhalenius@yahoo.se>
2019-10-02 20:49:55 +0200

    finalized UserSettingsTests

--- UnitTests/UserSettingsTests.cs
+++ UnitTests/UserSettingsTests.cs
@@ -1,16 +1,68 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrystalClear.Scripting;
 
 namespace UnitTests
 {
 	public class UserSettingsTests
 	{
 		[TestClass]
-		public class CreateNewSettingTest
+		public class SetUpUserSettingsTest
 		{
 			[TestMethod]
-			public void CreateNewSetting()
+			public void SetUpUserSettings()
 			{
-				object ObjectToStore = "";
+				UserSettings.SetUp();
+				Assert.IsTrue(UserSettings.IsSetUp());
+				UserSettings.DeleteAllSettings();
+			}
+		}
+
+		[TestClass]
+		public class CreateAndReadSettingTest
+		{
+			[TestMethod]
+			public void CreateAndReadSetting()
+			{
+				object ObjectToStore = "If you can read this, computer, then the save and load has been successful!";
+				string NameForSetting = "UnitTestSetting";
+
+				UserSettings.SetUp();
+				UserSettings.SaveSetting(NameForSetting, ObjectToStore);
+
+				object ResultingObjectAfterLoad = UserSettings.GetSetting(NameForSetting).value;
+
+				Assert.AreEqual(ObjectToStore, ResultingObjectAfterLoad);
+
+				UserSettings.DeleteAllSettings();
+			}
+		}
+
+		[TestClass]
+		public class DeleteUserSettingsTestss
+		{
+			[TestMethod]
+			public void DeleteAllUserSettings()
+			{
+				UserSettings.SetUp();
+				UserSettings.DeleteAllSettings();
+				Assert.IsFalse(UserSettings.IsSetUp());
+			}
+
+			[TestMethod]
+			public void DeleteSpecificUserSetting()
+			{
+				string NameOfObjectToStore = "DeleteSpecificUserSetting UserSetting";
+				string ObjectToStore = "123 ABC";
+
+				UserSettings.SetUp();
+
+				UserSettings.SaveSetting(NameOfObjectToStore, ObjectToStore);
+				Assert.IsTrue(UserSettings.ExistsSetting(NameOfObjectToStore));
+
+				UserSettings.DeleteSetting(NameOfObjectToStore);
+
+				Assert.IsFalse(UserSettings.ExistsSetting(NameOfObjectToStore));
+				UserSettings.DeleteAllSettings();
 			}
 		}
 	}
