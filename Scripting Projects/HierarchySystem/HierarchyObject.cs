b09f4dba27d44c66473a0872a50578baf7522513
Pixxl <oliverhalenius@yahoo.se>
2019-11-19 15:45:04 +0100

    documented code fixed error, likely persisting from before HierarchyRoots were HierarchyObjects! improved UnitTest of FollowPath slightly

--- Scripting Projects/HierarchySystem/HierarchyObject.cs
+++ Scripting Projects/HierarchySystem/HierarchyObject.cs
@@ -8,56 +8,72 @@ namespace CrystalClear.HierarchySystem
 	/// </summary>
 	public abstract class HierarchyObject
 	{
-		public HierarchyObject(HierarchyObject hierarchyRoot = null, HierarchyObject parent = null)
+		public HierarchyObject(HierarchyObject root = null, HierarchyObject parent = null)
 		{
-			root = hierarchyRoot;
+			hierarchyRoot = root;
 			this.parent = parent;
 		}
 
-		public bool IsAtRoot
+		/// <summary>
+		/// Is this HierarchyObject at the root of the hierarchy?
+		/// </summary>
+		public bool IsRoot
 		{
 			get
 			{
-				return (parent == null); // We know that we are the root if our parent is null (parent has to be a HierarchyRoot)
+				if (HierarchySystem.LoadedHierarchies.ContainsValue(this)) // Extra check that this simply wasnt incorrectly initialized
+				{
+					return (parent == null); // If this HierarchyObject has no parent that means that it has to be at the root, since HierarchySystem cannot be used as parent
+				}
+				else
+					return false;
 			}
 		}
 
-		private HierarchyObject root;
+		/// <summary>
+		/// The private field referencing this Hierarchy´s root.
+		/// </summary>
+		private HierarchyObject hierarchyRoot;
+		/// <summary>
+		/// Returns the root of this Hierarchy.
+		/// </summary>
 		public HierarchyObject HierarchyRoot
 		{
-			get => root;
+			get => hierarchyRoot;
 		}
 
+		/// <summary>
+		/// Returns the name of the containing hierarchy, or calls the HierarchySystem´s rename method if set.
+		/// </summary>
 		public string HierarchyName
 		{
-			get => root.Name;
+			get => HierarchySystem.GetName(HierarchyRoot);
 			set
 			{
 				HierarchySystem.SetName(HierarchyRoot, value);
 			}
 		}
 
+		/// <summary>
+		/// The field referencing this HierarchyObject´s parent in the Hierarchy.
+		/// </summary>
 		private HierarchyObject parent;
+		/// <summary>
+		/// Returns the parent, but does not allow setting it.
+		/// </summary>
 		public object Parent
 		{
-			get
-			{
-				if (IsAtRoot == false)
-				{
-					return parent;
-				}
-				else
-				{
-					return HierarchyRoot;
-				}
-			}
+			get => parent;
 		}
 
+		/// <summary>
+		/// Returns the name of this HierarchyObject by looking it up via GetName() on the parent (or HierarchySystem if this HierarchyObject is the root). This property supports setting the name, which uses the SetName() method on the parent or HierarchySystem if this Hierarchy object is the root.
+		/// </summary>
 		public string Name
 		{
 			get
 			{
-				if (IsAtRoot == false)
+				if (IsRoot == false)
 				{
 					return parent.GetName(this);
 				}
@@ -69,6 +85,11 @@ namespace CrystalClear.HierarchySystem
 			set => parent.SetName(this, value);
 		}
 
+		/// <summary>
+		/// This method sets the name of this 
+		/// </summary>
+		/// <param name="hierarchyObject">The HierarchyObject that should recieve a name change</param>
+		/// <param name="newName">The new name for the object</param>
 		private void SetName(HierarchyObject hierarchyObject, string newName)
 		{
 			string key = localHierarchy.FirstOrDefault(x => x.Value == hierarchyObject).Key;
@@ -76,6 +97,11 @@ namespace CrystalClear.HierarchySystem
 			localHierarchy.Add(newName, hierarchyObject);
 		}
 
+		/// <summary>
+		/// Gets the name of a child HierarchyObject.
+		/// </summary>
+		/// <param name="hierarchyObject">The HierarchyObject to get the name of</param>
+		/// <returns>The name of this object</returns>
 		private string GetName(HierarchyObject hierarchyObject)
 		{
 			string key;
@@ -83,32 +109,52 @@ namespace CrystalClear.HierarchySystem
 			return key;
 		}
 
+		/// <summary>
+		/// The entire current hierarchy from the root, for users modifying pleasure.
+		/// </summary>
 		public Dictionary<string, HierarchyObject> Hierarchy
 		{
 			get => HierarchyRoot.LocalHierarchy;
 		}
 
-		private Dictionary<string, HierarchyObject> localHierarchy = new Dictionary<string, HierarchyObject>();
-		public /*we want to make this effectively readonly*/ Dictionary<string, HierarchyObject> LocalHierarchy => localHierarchy;
+		/// <summary>
+		/// The local hierarchy, containing all child HierarchyObjects that this HierarchyObject has.
+		/// </summary>
+		protected Dictionary<string, HierarchyObject> localHierarchy = new Dictionary<string, HierarchyObject>();
+		/// <summary>
+		/// The public LocalHierarchy. It containins all child HierarchyObjects that this HierarchyObject has.
+		/// </summary>
+		public Dictionary<string, HierarchyObject> LocalHierarchy => localHierarchy;
 
-		public void Add(string name, HierarchyObject hierarchyObject)
+		/// <summary>
+		/// Adds the HierarchyObject and changes it´s parent.
+		/// </summary>
+		/// <param name="name">The name to use in the reparent</param>
+		/// <param name="hierarchyObject">The HierarchyObject to reparent</param>
+		public void ReParent(string name, HierarchyObject hierarchyObject)
 		{
 			LocalHierarchy.Add(name, hierarchyObject);
+			hierarchyObject.parent = this;
 		}
 
+		/// <summary>
+		/// Follows a path, relatively from this point.
+		/// </summary>
+		/// <param name="path">The path to follow. HierarchyObjects are separated by '/'</param>
+		/// <returns>The HierarchyObject at the end of the path</returns>
 		public HierarchyObject FollowPath(string path)
 		{
-			string[] pathSegments = path.Split('/');
+			string[] pathSegments = path.Split('/'); // Split the path into the individual HierarchyObjects to follow 
 
-			if (pathSegments.Length == 1) // We have reached the end of the path, this is the destination!
+			if (pathSegments.Length == 1) // We have reached the end of the path, this HierarchyObject is the destination!
 			{
-				return this;
+				return this; // Return this HierarchyObject
 			}
 
-			string pathToFollow = path.Remove(0, pathSegments[0].Length + 1);
-			string nextObject = pathSegments[1];
+			string pathToFollow = path.Remove(0, pathSegments[0].Length + 1); // Remove the top level HierarchyObject from the path as we continue forwards
+			string nextObject = pathSegments[1]; // The next HierarchyObject´s name
 
-			return localHierarchy[nextObject].FollowPath(pathToFollow);
+			return localHierarchy[nextObject].FollowPath(pathToFollow); // We are going to do some of that sweet bitter sweet recursion magic by returning the result of a follow call to the HierarchyObject that is next in the path.
 		}
 	}
 }
--- UnitTesting/HierarchySystem/HierarchySystemUnitTests.cs
+++ UnitTesting/HierarchySystem/HierarchySystemUnitTests.cs
@@ -8,7 +8,7 @@ namespace UnitTesting
 	public class HierarchySystemUnitTest
 	{
 		/// <summary>
-		/// Initialize needs to run before every test
+		/// Initialize needs to run before every test.
 		/// </summary>
 		[TestInitialize]
 		public void Initialize()
@@ -18,23 +18,29 @@ namespace UnitTesting
 			WorldObject ho2 = new WorldObject();
 			UIObject ho33 = new UIObject();
 			UIObject ho3 = new UIObject();
-			ho3.Add("ho3.3", ho33);
-			hierarchyRoot.Add("ho1", ho1);
-			hierarchyRoot.Add("ho2", ho2);
-			hierarchyRoot.Add("ho3", ho3);
+			ho3.ReParent("ho3.3", ho33);
+			hierarchyRoot.ReParent("ho1", ho1);
+			hierarchyRoot.ReParent("ho2", ho2);
+			hierarchyRoot.ReParent("ho3", ho3);
 			HierarchySystem.Add("test", hierarchyRoot);
 		}
 
+		/// <summary>
+		/// Tests the FollowPath system
+		/// </summary>
 		[TestMethod]
 		public void TestFollowPath() // Test for making sure the follow path system works
 		{
 			HierarchyObject hierarchyObjectToFind = new ScriptObject();
-			HierarchySystem.LoadedHierarchies["test"].Add("followPathTest", hierarchyObjectToFind);
+			HierarchyObject hierarchyObjectToFind2 = new UIObject();
+			HierarchySystem.LoadedHierarchies["test"].ReParent("followPathTest", hierarchyObjectToFind);
+			HierarchySystem.LoadedHierarchies["test"].LocalHierarchy["followPathTest"].ReParent("followPathTest2", hierarchyObjectToFind2);
 			Assert.AreEqual(hierarchyObjectToFind, HierarchySystem.FollowPath("test/followPathTest"));
+			Assert.AreEqual(hierarchyObjectToFind2, HierarchySystem.FollowPath("test/followPathTest/followPathTest2"));
 		}
 
 		/// <summary>
-		/// Needs to be run after every test
+		/// Cleanup to be run after every test.
 		/// </summary>
 		[TestCleanup]
 		public void Cleanup()
