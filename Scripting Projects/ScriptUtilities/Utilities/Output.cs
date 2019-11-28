7daa0f854ac22df076cc3c9daeec1edfdb747a28
Pixxl <oliverhalenius@yahoo.se>
2019-11-28 18:24:02 +0100

    add documentation

--- Scripting Projects/ScriptUtilities/Utilities/Output.cs
+++ Scripting Projects/ScriptUtilities/Utilities/Output.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CrystalClear.ScriptUtilities
 {
 	public static class Output
 	{
+		/// <summary>
+		/// Write an array of objects to the console or log.
+		/// </summary>
+		/// <param name="objs">The objects to write.</param>
 		public static void Log(object[] objs)
 		{
 			foreach (object obj in objs)
@@ -12,26 +18,50 @@ namespace CrystalClear.ScriptUtilities
 			}
 		}
 
+		/// <summary>
+		/// Write a collection of objects to the console or log.
+		/// </summary>
+		/// <param name="objs">The objects to write.</param>
+		public static void Log(ICollection objs)
+		{
+			foreach (object obj in objs)
+			{
+				Console.WriteLine(obj);
+			}
+		}
+
+		/// <summary>
+		/// Write an object to the console or log.
+		/// </summary>
+		/// <param name="obj">The object to write.</param>
 		public static void Log(object obj)
 		{
 			Console.WriteLine(obj);
 		}
 
+		/// <summary>
+		/// Write a string to the console or log.
+		/// </summary>
+		/// <param name="str">The string to write.</param>
 		public static void Log(string str)
 		{
 			Console.WriteLine(str);
 		}
 
+		/// <summary>
+		/// Writes a string to the output with additional settings for background color and foreground color.
+		/// </summary>
+		/// <param name="str"></param>
+		/// <param name="bgColor"></param>
+		/// <param name="fgColor"></param>
 		public static void Log(string str, ConsoleColor bgColor, ConsoleColor fgColor)
 		{
-			ConsoleColor
-				prevFgColor =
-					Console.ForegroundColor; // Store previous foreground and background color so that we can restore them after writing
+			ConsoleColor prevFgColor = Console.ForegroundColor; // Store previous foreground and background color so that we can restore them after writing.
 			ConsoleColor prevBgColor = Console.BackgroundColor;
-			Console.BackgroundColor = bgColor; // Set new colors
+			Console.BackgroundColor = bgColor; // Set new colors.
 			Console.ForegroundColor = fgColor;
-			Console.WriteLine(str); // Write string
-			Console.BackgroundColor = prevBgColor; // Restore previous colors
+			Console.WriteLine(str); // Write string.
+			Console.BackgroundColor = prevBgColor; // Restore previous colors.
 			Console.ForegroundColor = prevFgColor;
 		}
 	}
