3d9e359a0e28320b3a8b43ca99534602804cd617
Pixxl <oliverhalenius@yahoo.se>
2019-12-11 19:38:18 +0100

    documentation!

--- Scripting Projects/Standard/HierarchyObjects/WorldObject.cs
+++ Scripting Projects/Standard/HierarchyObjects/WorldObject.cs
@@ -37,15 +37,21 @@ namespace CrystalClear.Standard.HierarchyObjects
 
 		protected override void OnLocalHierarchyChange()
 		{
+			// Initialize a list for temporarily containging the child transforms.
 			List<Transform> childTransforms = new List<Transform>();
+			// Iterate through the children in the localHierarchy.
 			foreach (HierarchyObject child in LocalHierarchy.Values)
 			{
+				// Initialize a new transform to store the transform of the child (if any).
 				Transform transform = (child as WorldObject).Transform;
+				// Did the child have a transform?
 				if (transform != null)
 				{
+					// Add the child's transform to the child transforms!
 					childTransforms.Add(Transform);
 				}
 			}
+			// Set the child transforms to the temporary child transform.
 			Transform.Children = childTransforms.ToArray();
 		}
 
@@ -57,20 +63,45 @@ namespace CrystalClear.Standard.HierarchyObjects
 		}
 	}
 
+	/// <summary>
+	/// A Transform is an object that keeps track of, and manages numerous functions related to the transformation and position, rotation and scaling properties of an object.
+	/// </summary>
 	public sealed class Transform
 	{
+		/// <summary>
+		/// Constructs a Transform with the specified position, rotation, scale, parent and children.
+		/// </summary>
+		/// <param name="position">The global position to use for this Transform.</param>
+		/// <param name="rotation">The global rotation to use for this Transform.</param>
+		/// <param name="scale">The global scale to use for this Transform.</param>
+		/// <param name="parent">The parent to use for this Transform.</param>
+		/// <param name="children">The children to use for this Transform.</param>
 		public Transform(Vector position, Vector rotation, Vector scale, Transform parent = null, Transform[] children = null)
 		{
+			// Set the global position.
 			globalPosition = position;
+			// Set the global rotation.
 			globalRotation = rotation;
+			// Set the global scale.
 			globalScale = scale;
+			// Set the parent.
 			Parent = parent;
+			// Set the children.
 			Children = children;
 		}
 
+		/// <summary>
+		/// The parent of this Transform.
+		/// </summary>
 		public Transform Parent;
+		/// <summary>
+		/// The children of this Transform.
+		/// </summary>
 		public Transform[] Children;
 
+		/// <summary>
+		/// The global position of this Transform.
+		/// </summary>
 		private Vector globalPosition;
 		public Vector GlobalPosition
 		{
@@ -88,8 +119,14 @@ namespace CrystalClear.Standard.HierarchyObjects
 			}
 		}
 
+		/// <summary>
+		/// The position of this Transform relative to it's parent's.
+		/// </summary>
 		public Vector LocalPosition => Parent.GlobalPosition - GlobalPosition;
 
+		/// <summary>
+		/// The global rotation of this Transform.
+		/// </summary>
 		private Vector globalRotation;
 		public Vector GlobalRotation
 		{
@@ -107,8 +144,14 @@ namespace CrystalClear.Standard.HierarchyObjects
 			}
 		}
 
+		/// <summary>
+		/// The rotation of this Transform relative to it's parent's.
+		/// </summary>
 		public Vector LocalRotation => Parent.GlobalRotation - GlobalRotation;
 
+		/// <summary>
+		/// The global scale of this Transform.
+		/// </summary>
 		private Vector globalScale;
 		public Vector GlobalScale
 		{
@@ -126,6 +169,9 @@ namespace CrystalClear.Standard.HierarchyObjects
 			}
 		}
 
+		/// <summary>
+		/// The scale of this Transform relative to it's parent's.
+		/// </summary>
 		public Vector LocalScale => Parent.GlobalScale - GlobalScale;
 	}
 
@@ -150,14 +196,20 @@ namespace CrystalClear.Standard.HierarchyObjects
 		}
 
 		/// <summary>
-		/// Initializes a clone of a vector.
+		/// Initializes a clone of a Vector.
 		/// </summary>
-		/// <param name="vector">The vector to clone.</param>
+		/// <param name="vector">The Vector to clone.</param>
 		public Vector(Vector vector)
 		{
 			this = vector;
 		}
 
+		/// <summary>
+		/// The subtraction operator for the Vector. The difference is a new Vector with all it's axis equal to the difference of the corresponding axis of a - b.
+		/// </summary>
+		/// <param name="a">The first Vector.</param>
+		/// <param name="b">The second Vector.</param>
+		/// <returns>The difference Vector.</returns>
 		public static Vector operator - (Vector a, Vector b)
 		{
 			if (a.Axis.Length != b.Axis.Length)
@@ -175,6 +227,12 @@ namespace CrystalClear.Standard.HierarchyObjects
 			return difference;
 		}
 
+		/// <summary>
+		/// The addition operator for the Vector. The sum is a new Vector with all it's axis equal to the sum of the corresponding axis of a + b.
+		/// </summary>
+		/// <param name="a">The first Vector.</param>
+		/// <param name="b">The second Vector.</param>
+		/// <returns>The sum Vector.</returns>
 		public static Vector operator + (Vector a, Vector b)
 		{
 			if (a.Axis.Length != b.Axis.Length)
@@ -192,6 +250,12 @@ namespace CrystalClear.Standard.HierarchyObjects
 			return sum;
 		}
 
+		/// <summary>
+		/// The division operator for the Vector. The quotient is a new Vector with all it's axis equal to the quotient of the corresponding axis of a / b.
+		/// </summary>
+		/// <param name="a">The first Vector.</param>
+		/// <param name="b">The second Vector.</param>
+		/// <returns>The quotient Vector.</returns>
 		public static Vector operator / (Vector a, Vector b)
 		{
 			if (a.Axis.Length != b.Axis.Length)
@@ -199,16 +263,22 @@ namespace CrystalClear.Standard.HierarchyObjects
 				throw new System.Exception("The axis of the two vectors are not comparable.");
 			}
 
-			Vector product = new Vector(a.Axis);
+			Vector quotient = new Vector(a.Axis);
 
-			for (int i = 0; i < product.Axis.Length; i++)
+			for (int i = 0; i < quotient.Axis.Length; i++)
 			{
-				product.Axis[i] = a.Axis[i] / b.Axis[i];
+				quotient.Axis[i] = a.Axis[i] / b.Axis[i];
 			}
 
-			return product;
+			return quotient;
 		}
 
+		/// <summary>
+		/// The multiplication operator for the Vector. The product is a new Vector with all it's axis equal to the product of the corresponding axis of a * b.
+		/// </summary>
+		/// <param name="a">The first Vector.</param>
+		/// <param name="b">The second Vector.</param>
+		/// <returns>The product Vector.</returns>
 		public static Vector operator * (Vector a, Vector b)
 		{
 			if (a.Axis.Length != b.Axis.Length)
