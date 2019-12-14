e78060558575a13b82c09b5bd86b51d60bbff2d2
Pixxl <oliverhalenius@yahoo.se>
2019-12-14 20:52:12 +0100

    improve documentation

--- Scripting Projects/ScriptingEngine/Compiler.cs
+++ Scripting Projects/ScriptingEngine/Compiler.cs
@@ -19,7 +19,7 @@ namespace CrystalClear.CompilationSystem
 		{
 			using (CSharpCodeProvider csProvider = new CSharpCodeProvider())
 			{
-				// Set the options for the compilation
+				// Set the options for the compilation.
 				CompilerParameters options = new CompilerParameters
 				{
 					GenerateExecutable = false,
@@ -27,35 +27,35 @@ namespace CrystalClear.CompilationSystem
 					TempFiles = new TempFileCollection(Environment.CurrentDirectory, false)
 				};
 
-				// The collection of references
+				// The collection of references.
 				string[] references =
 				{
 					@"E:\dev\crystal clear\Scripting Projects\ScriptUtilities\bin\Debug\ScriptUtilities.dll", // The path to the ScriptUtilities dll
 					@"E:\dev\crystal clear\Scripting Projects\EventSystem\bin\Debug\EventSystem.dll", // The path to the EventSystem dll
 					@"E:\dev\crystal clear\Scripting Projects\HierarchySystem\bin\Debug\HierarchySystem.dll", // The path to the EventSystem dll
 					@"E:\dev\crystal clear\Scripting Projects\Standard\bin\Debug\Standard.dll", // The path to the Standard dll
-					Assembly.GetExecutingAssembly().Location // The location of the ScriptingEngine
+					Assembly.GetExecutingAssembly().Location // The location of the CompilationSystem.
 				};
 
-				// Set references for the compiled code
+				// Set references for the compiled code.
 				options.ReferencedAssemblies.AddRange(references);
 
-				// Compile our code
+				// Compile our code.
 				CompilerResults result = csProvider.CompileAssemblyFromFile(options, fileNames);
 
-				// Iterate through all errors and report them to the user
+				// Iterate through all errors and report them to the user.
 				foreach (object error in result.Errors)
 				{
 					Console.WriteLine(error);
 				}
 
-				// If we have errors then return null
+				// If we have errors then return null.
 				if (result.Errors.HasErrors)
 				{
 					return null;
 				}
-
-				// Return our successfull compiled assembly
+				
+				// Return our successfull compiled assembly.
 				return result.CompiledAssembly;
 			}
 		}
