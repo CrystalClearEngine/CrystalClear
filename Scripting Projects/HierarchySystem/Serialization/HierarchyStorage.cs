c567722271a39df2d5fc27cf5f870adc17aee2ff
Pixxl <oliverhalenius@yahoo.se>
2019-12-23 00:48:41 +0100

    add HierarchyStorage and Serialization folder to HierarchySystem

---
+++ Scripting Projects/HierarchySystem/Serialization/HierarchyStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace CrystalClear.HierarchySystem
+{
+	public class HierarchyStorage
+	{
+		
+	}
+}
