ccc09c51d548fc8f55580288f3bedc5a96f82ce9
Pixxl <oliverhalenius@yahoo.se>
2019-12-23 14:04:37 +0100

    preparing ScriptStorage for DataContract serializer.

--- Scripting Projects/HierarchySystem/Serialization/ScriptStorage.cs
+++ Scripting Projects/HierarchySystem/Serialization/ScriptStorage.cs
@@ -1,19 +1,45 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 
 namespace CrystalClear.HierarchySystem.Scripting
 {
 	/// <summary>
 	/// ScriptStorage is a type specifically for allowing serialization and deserialization of Scripts.
 	/// </summary>
+	[DataContract(Name = "Script")]
 	[Serializable] // For the binary formatter...
 	public class ScriptStorage
 	{
+		public ScriptStorage(Type scriptType, object[] constructorParameters = null, HierarchyObject attatchedTo = null)
+		{
+			this.assemblyQualifiedTypeName = scriptType.AssemblyQualifiedName;
+			this.constructorParameters = constructorParameters;
+			this.attatchedToPath = attatchedTo.Path;
+		}
+
+		#region Data
 		/// <summary>
 		/// The assemblyQualifiedName of the type of the Script.
 		/// </summary>
+		[DataMember(Name = "ScriptTypeName")]
 		private readonly string assemblyQualifiedTypeName;
+
+		/// <summary>
+		/// The parameters to use when constructing this Script.
+		/// </summary>
+		[DataMember(Name = "ConstructorParameters")]
+		private readonly object[] constructorParameters;
+
+		/// <summary>
+		/// The path to follow from HierarchyManager to find the HierarchyObject this Script is attatched to.
+		/// </summary>
+		[DataMember(Name = "AttatchedToPath")]
+		private readonly string attatchedToPath; // TODO/REM Shouldn't HierarchyObjects have the job of attatching the script? Regardless this should still stay since others could get use out of it.
+		#endregion
+
+		#region Properties
 		public Type Type
 		{
 			get
@@ -29,15 +55,6 @@ namespace CrystalClear.HierarchySystem.Scripting
 			}
 		}
 
-		/// <summary>
-		/// The parameters to use when constructing this Script.
-		/// </summary>
-		private readonly object[] constructorParameters;
-
-		/// <summary>
-		/// The path to follow from HierarchyManager to find the HierarchyObject this Script is attatched to.
-		/// </summary>
-		private readonly string attatchedToPath; // TODO/REM Shouldn't HierarchyObjects have the job of attatching the script? Regardless this should still stay since others could get use out of it.
 		public HierarchyObject AttatchedTo
 		{
 			get
@@ -45,29 +62,9 @@ namespace CrystalClear.HierarchySystem.Scripting
 				return HierarchyManager.FollowPath(attatchedToPath);
 			}
 		}
+		#endregion
 
-		public ScriptStorage(Type scriptType, object[] constructorParameters = null, HierarchyObject attatchedTo = null)
-		{
-			this.assemblyQualifiedTypeName = scriptType.AssemblyQualifiedName;
-			this.constructorParameters = constructorParameters;
-			this.attatchedToPath = attatchedTo.Path;
-		}
-
-		/// <summary>
-		/// Serializes and writes the ScriptStorage to a binary file using the BinaryFormatter.
-		/// </summary>
-		/// <param name="path">The path to store the ScriptStorage to.</param>
-		/// <param name="toStore">The ScriptStorage to store.</param>
-		public static void StoreToFile(string path, ScriptStorage toStore)
-		{
-			using (FileStream stream = new FileStream(path, FileMode.Create))
-			{
-				BinaryFormatter binaryFormatter = new BinaryFormatter();
-
-				binaryFormatter.Serialize(stream, toStore);
-			}
-		}
-
+		#region Creators
 		/// <summary>
 		/// Deserializes the ScriptStorage from the provided binary file.
 		/// </summary>
@@ -102,5 +99,23 @@ namespace CrystalClear.HierarchySystem.Scripting
 			Script script = new Script(Type, constructorParameters, AttatchedTo); //TODO look up wether or not declaring a variable like this wastes any memory or if it is optimized. This does look cleaner than just returning I think, so for now it stays here and everywhere else!
 			return script;
 		}
+		#endregion
+
+		#region Storing
+		/// <summary>
+		/// Serializes and writes the ScriptStorage to a binary file using the BinaryFormatter.
+		/// </summary>
+		/// <param name="path">The path to store the ScriptStorage to.</param>
+		/// <param name="toStore">The ScriptStorage to store.</param>
+		public static void StoreToFile(string path, ScriptStorage toStore)
+		{
+			using (FileStream stream = new FileStream(path, FileMode.Create))
+			{
+				BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+				binaryFormatter.Serialize(stream, toStore);
+			}
+		}
+		#endregion
 	}
 }
