7d85e4605844220bb0d6cb4ca930823ccb15dd9e
Pixxl <oliverhalenius@yahoo.se>
2020-02-13 21:46:00 +0100

    add UnitTests for ImaginaryObjects (WIP)

---
+++ UnitTesting/Serialization Tests/ImaginaryObjectTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using CrystalClear.SerializationSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+	class TestObject : IEquatable<TestObject>
+	{
+		public TestObject(string stringData)
+		{
+			StringData = stringData;
+		}
+
+		public string StringData;
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as TestObject);
+		}
+
+		public bool Equals(TestObject other)
+		{
+			return other != null &&
+				   StringData == other.StringData;
+		}
+
+		public override int GetHashCode()
+		{
+			return 1045584480 + EqualityComparer<string>.Default.GetHashCode(StringData);
+		}
+
+		public static bool operator ==(TestObject left, TestObject right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(TestObject left, TestObject right)
+		{
+			return !(left == right);
+		}
+	}
+
+	[TestCategory("ImaginaryObjects")]
+	[TestClass]
+	public class ImaginaryObjectTests
+	{
+		[TestMethod]
+		public void TestImaginaryObject()
+		{
+			TestObject testObject = new TestObject("Hello!");
+
+			ImaginaryObject imaginaryObject = new ImaginaryObject(typeof(TestObject),
+														 new ImaginaryObject[] { new ImaginaryPrimitive("Hello!") });
+
+			Assert.IsTrue(testObject == (TestObject)imaginaryObject.CreateInstance());
+
+			Assert.AreEqual(testObject, (TestObject)imaginaryObject.CreateInstance());
+		}
+	}
+}
