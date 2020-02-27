7db833611a50d174b1cd293c4e8b096db7069306
Pixxl <oliverhalenius@yahoo.se>
2020-02-27 21:14:06 +0100

    add ReflectiveEquals method to Utilities in CrystalClear! also add UnitTests for them (WIP

--- CrystalClear/Utilities.cs
+++ CrystalClear/Utilities.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace CrystalClear
 {
@@ -30,6 +32,90 @@ namespace CrystalClear
 			}
 
 			return baseName;
+		}
+
+		public static bool ReflectionEquals(this object a, object b)
+		{
+			// A and b cannot be compared if their types are not compatible.
+			if (b.GetType().IsAssignableFrom(a.GetType()))
+			{
+				//b = Convert.ChangeType(b, a.GetType()); // WORKING ON ACTUALLY ALLOWING ASSIGNABLES TO BE COMPREAD
+				return false;
+			}
+			// We know for sure that we don't need to do any other checking if a and b are the same instances.
+			else if (ReferenceEquals(a, b))
+			{
+				return true;
+			}
+
+			FieldInfo[] fields = a.GetType().GetFields();
+			PropertyInfo[] properties = a.GetType().GetProperties();
+
+			// Iterate through all fields to compare them.
+			for (int i = 0; i < fields.Length; i++)
+			{
+				// Store field A and B in variables.
+				object fieldA = fields[i].GetValue(a);
+				object fieldB = fields[i].GetValue(b);
+
+				// Is fieldA (and fieldB as well, since they are of the same type) primitive or Equatable? If so, we can compare them!
+				if (fieldA.GetType().IsPrimitive || fieldA.GetType().GetInterfaces().Any(x =>
+						x.IsGenericType &&
+						x.GetGenericTypeDefinition() == typeof(IEquatable<>)))
+				{
+					// Are the A and B fields at this index non-equal?
+					if (!fieldA.Equals(fieldB))
+					{
+						// Return the method with false, as no other checks need to be done if even just one field is different.
+						return false;
+					}
+					else
+						continue;
+				}
+
+				// Are the A and B fields at this index non-(reflectively)equal?
+				else if (!ReflectionEquals(fieldA, fieldB))
+				{
+					// Return the method with false, as no other checks need to be done if even just one field is different.
+					return false;
+				}
+
+				// If A and B are equal, we can continue.
+			}
+
+			// Iterate through all properties to compare them.
+			for (int i = 0; i < properties.Length; i++)
+			{
+				// Store property A and B in variables.
+				object propertyA = properties[i].GetValue(a);
+				object propertyB = properties[i].GetValue(b);
+
+				// Is propertyA (and propertyB as well, since they are of the same type) primitive or Equatable? If so, we can compare them!
+				if (propertyA.GetType().IsPrimitive || propertyA.GetType().GetInterfaces().Any(x =>
+						x.IsGenericType &&
+						x.GetGenericTypeDefinition() == typeof(IEquatable<>)))
+				{
+					// Are the A and B properties at this index non-equal?
+					if (!propertyA.Equals(propertyB))
+					{
+						// Return the method with false, as no other checks need to be done if even just one property is different.
+						return false;
+					}
+					else
+						continue;
+				}
+
+				// Are the A and B properties at this index non-(reflectively)equal?
+				else if (!ReflectionEquals(propertyA, propertyB))
+				{
+					// Return the method with false, as no other checks need to be done if even just one property is different.
+					return false;
+				}
+
+				// If A and B are equal, we can continue.
+			}
+
+			return true;
 		}
 	}
 }
---
+++ UnitTesting/CrystalClear Utilities Tests/CrystalClearUtilitiesTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static CrystalClear.Utilities;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class CrystalClearUtilitiesTests
+	{
+		[TestMethod]
+		public void SimpleReflectionEqualsTest()
+		{
+			{
+				TestObject testObjectA = new TestObject("Hello!");
+				TestObject testObjectB = new TestObject("Hello!");
+
+				Assert.IsTrue(testObjectA.ReflectionEquals(testObjectB), "If this check failed, that means that ReflectionEquals recognized two equal TestObjects as non-equal.");
+			}
+
+			{
+				TestObject testObjectA = new TestObject("Hello!");
+				TestObject testObjectB = new TestObject("This is a different string.");
+
+				Assert.IsFalse(testObjectA.ReflectionEquals(testObjectB), "If this check failed, that means that ReflectionEquals recognized two non-equal TestObjects as equal.");
+			}
+		}
+	}
+}
