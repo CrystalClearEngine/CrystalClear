57dff4a6aa8f93133e0561c1be1b3c02e1691a25
Pixxl <oliverhalenius@yahoo.se>
2020-04-20 23:21:00 +0200

    add IsRunning and checks using it in RuntimeMain and also add Stop method

--- RuntimeMain/RuntimeMain.cs
+++ RuntimeMain/RuntimeMain.cs
@@ -10,6 +10,8 @@ namespace CrystalClear.RuntimeMain
 {
 	public static class RuntimeMain
 	{
+		public static bool IsRunning = false;
+
 		public static void SubscribeAll(params Assembly[] userAssemblies)
 		{
 			EventSystem.EventSystem.FindAndSubscribeEventMethods(Assembly.GetAssembly(typeof(ScriptObject)));
@@ -22,11 +24,21 @@ namespace CrystalClear.RuntimeMain
 
 		public static void Run(string hierarchyPath, string hierarchyName)
 		{
+			if (IsRunning)
+			{
+				throw new Exception("Already running!");
+			}
+
 			Run(hierarchyName, ImaginaryObjectSerialization.UnpackHierarchyFromFile(hierarchyPath));
 		}
 
 		public static void Run(string hierarchyName, ImaginaryHierarchyObject rootHierarchyObject)
 		{
+			if (IsRunning)
+			{
+				throw new Exception("Already running!");
+			}
+
 			#region Creating
 			#region Profiling
 			Stopwatch performanceStopwatchForCreate = new Stopwatch();
@@ -44,8 +56,25 @@ namespace CrystalClear.RuntimeMain
 
 		public static void Run()
 		{
+			if (IsRunning)
+			{
+				throw new Exception("Already running!");
+			}
+
+			IsRunning = true;
+
 			// Raise the start event.
 			StartEvent.Instance.RaiseEvent();
+		}
+
+		public static void Stop()
+		{
+			if (!IsRunning)
+			{
+				throw new Exception("Not running!");
+			}
+
+			IsRunning = false;
 		}
 	}
 }
