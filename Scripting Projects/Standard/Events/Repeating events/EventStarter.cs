39541b8cf07dc82e0c0e2c11f942da614e1ec8ac
Pixxl <oliverhalenius@yahoo.se>
2020-04-20 23:24:21 +0200

    organize Standard

--- Scripting Projects/Standard/Events/Repeating events/EventStarter.cs
+++ Scripting Projects/Standard/Events/Repeating events/EventStarter.cs
@@ -5,28 +5,40 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using CrystalClear.EventSystem.StandardEvents;
 
 namespace CrystalClear.Standard.Events
 {
 	public static class EventStarter
 	{
+		public static Thread frameUpdateThread;
+		public static Thread physicsUpdateThread;
+		public static Thread inputPollingThread;
+
 		[OnStartEvent]
 		public static void StartEvents()
 		{
 			// Create a thread for updating the frame.
-			Thread frameUpdateThread = new Thread(FrameUpdateEvent.FrameUpdateLoop);
+			frameUpdateThread = new Thread(FrameUpdateEvent.FrameUpdateLoop);
 			// Start the thread.
 			frameUpdateThread.Start();
 
 			// Create a thread for updating the physics' time-step.
-			Thread physicsUpdateThread = new Thread(() => PhysicsTimeStepEventClass.PhysicsTimeStepLoop());
+			physicsUpdateThread = new Thread(() => PhysicsTimeStepEventClass.PhysicsTimeStepLoop());
 			// Start the thread.
 			physicsUpdateThread.Start();
 
 			// Create a thread for polling input.
-			Thread inputPollingThread = new Thread(() => InputPollEvent.InputPollLoop());
+			inputPollingThread = new Thread(() => InputPollEvent.InputPollLoop());
 			// Start the thread.
 			inputPollingThread.Start();
+		}
+
+		public static void StopEvents()
+		{
+			frameUpdateThread.Abort();
+			physicsUpdateThread.Abort();
+			inputPollingThread.Abort();
 		}
 	}
 }
