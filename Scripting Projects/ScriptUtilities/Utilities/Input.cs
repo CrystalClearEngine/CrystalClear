ae344b1619be5da059081e89f51684760550dd62
Pixxl <oliverhalenius@yahoo.se>
2020-05-03 15:36:51 +0200

    move AskQuestion methods to Input

--- Scripting Projects/ScriptUtilities/Utilities/Input.cs
+++ Scripting Projects/ScriptUtilities/Utilities/Input.cs
@@ -16,4 +16,37 @@ namespace CrystalClear.ScriptUtilities.Utilities
 			while (true);
 		}
 	}
+
+	public static class ConsoleInput
+	{
+		public static bool AskYOrNQuestion(string question)
+		{
+			retry:
+			Console.Write(question + ": ");
+
+			switch (Console.ReadKey().KeyChar)
+			{
+				case 't':
+				case 'y':
+					Console.WriteLine();
+					return true;
+
+				case 'f':
+				case 'n':
+					Console.WriteLine();
+					return false;
+
+				default:
+					Console.WriteLine("Invalid!");
+					goto retry;
+			}
+		}
+
+		public static string AskQuestion(string question)
+		{
+			Console.Write(question + ": ");
+			string response = Console.ReadLine();
+			return response;
+		}
+	}
 }
