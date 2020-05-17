48406517766adddc21f6fbdb2372776f8afec7f8
Pixxl <oliverhalenius@yahoo.se>
2020-05-17 12:56:13 +0200

    WIP on Build command! [click for more info]
    Builder:
    - remove and sort usings
    - add parameters for execurtable name and user generated assemblies
    - add GenerateMainMethodCode
    - make sure to create buildDirectory
    - call the compiler and, well, compile
    
    CompilationSystem:
    - switch from net standard to net core 3.1
    
    Compiler:
    - improve code readability and variable naming
    - improve comments
    - add CompileExecutable method
    
    Main:
    - add TODOs
    - update build command to use build's new features
    - reenable error handling on certain exceptions

--- CompilationSystem/Builder.cs
+++ CompilationSystem/Builder.cs
@@ -1,15 +1,39 @@
-﻿using System.IO;
-using static CrystalClear.EditorInformation;
+﻿using CrystalClear.ScriptUtilities;
+using System.IO;
+using System.Reflection;
 
 namespace CrystalClear.CompilationSystem
 {
 	public static class Builder
 	{
-		public static void Build(string buildPath)
+		public static void Build(string buildPath, string exectutableName, Assembly[] userGeneratedAssemblies)
 		{
 			DirectoryInfo buildDirectory = new DirectoryInfo(buildPath);
+			buildDirectory.Create();
 
-			
+			if (!Compiler.CompileExecutable(GenerateMainMethodCode(startDefaultEvents: false), userGeneratedAssemblies, buildPath, exectutableName))
+			{
+				// If the build failed...
+				Output.ErrorLog("Compilation of executable failed :(, returning.");
+				return;
+			}
+
+			Output.Log($@"Successfully built {exectutableName} at location {buildPath}\{exectutableName}.exe.", System.ConsoleColor.Black, System.ConsoleColor.Green);
+		}
+
+		private static string GenerateMainMethodCode(bool startDefaultEvents, string hierarchyToLoadInitially = null, string mainClassName = "Program")
+		{
+			string mainMethodCode =
+			"using CrystalClear.RuntimeMain;" +
+			$"public static class {mainClassName}" +
+			"{" +
+			"	public static void Main(string[] args)" +
+			"	{" +
+			$"		RuntimeMain.Run({(hierarchyToLoadInitially is null ? string.Empty : hierarchyToLoadInitially)});" +
+			"	}" +
+			"}";
+
+			return mainMethodCode;
 		}
 	}
 }
