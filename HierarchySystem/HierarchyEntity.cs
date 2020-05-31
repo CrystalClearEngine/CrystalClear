a66a24ad01cd7e1f4f0736d3dacd9639c0f40dda
Pixxl <oliverhalenius@yahoo.se>
2020-05-31 12:40:15 +0200

    add equality methods to HierarchyEntity

--- HierarchySystem/HierarchyEntity.cs
+++ HierarchySystem/HierarchyEntity.cs
@@ -1,4 +1,6 @@
 ﻿using CrystalClear.ECS;
+using System;
+using System.Collections.Generic;
 
 namespace CrystalClear.HierarchySystem.ECS
 {
@@ -7,6 +9,7 @@ namespace CrystalClear.HierarchySystem.ECS
 	/// </summary>
 	public struct HierarchyEntity
 		: IEntity
+		, IEquatable<HierarchyEntity>
 	{
 		public int EntityId { get; }
 
@@ -15,5 +18,40 @@ namespace CrystalClear.HierarchySystem.ECS
 		public readonly Hierarchy<HierarchyEntity> LocalEntityHierarchy;
 
 		public readonly Hierarchy LocalHierarchy;
+
+		#region Equality Overrides
+		public override bool Equals(object obj)
+		{
+			return obj is HierarchyEntity entity && Equals(entity);
+		}
+
+		public bool Equals(HierarchyEntity other)
+		{
+			return EntityId == other.EntityId &&
+				   ParentEntityId == other.ParentEntityId &&
+				   LocalEntityHierarchy.Equals(other.LocalEntityHierarchy) &&
+				   EqualityComparer<Hierarchy>.Default.Equals(LocalHierarchy, other.LocalHierarchy);
+		}
+
+		public override int GetHashCode()
+		{
+			var hashCode = -2012399249;
+			hashCode = hashCode * -1521134295 + EntityId.GetHashCode();
+			hashCode = hashCode * -1521134295 + ParentEntityId.GetHashCode();
+			hashCode = hashCode * -1521134295 + LocalEntityHierarchy.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<Hierarchy>.Default.GetHashCode(LocalHierarchy);
+			return hashCode;
+		}
+
+		public static bool operator ==(HierarchyEntity left, HierarchyEntity right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(HierarchyEntity left, HierarchyEntity right)
+		{
+			return !(left == right);
+		}
+		#endregion
 	}
 }
