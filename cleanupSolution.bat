564df3881061534eaa8c87ef6cb065dd6fc418d3
Pixxl <oliverhalenius@yahoo.se>
2020-06-06 00:49:06 +0200

    remove redundant batch file

--- cleanupSolution.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-echo Hello! Welcome to the cleanup utility for Crystal Clear!
-echo This utility cleans the debug and release folders since they can fill up with temporary files.
-echo Close this program to cancel the cleanup or press any key to clean.
-pause
-echo Deleting Main\bin\Debug\...
-del Main\bin\Debug\
-echo Deleting Main\bin\Release\...
-del Main\bin\Release\
