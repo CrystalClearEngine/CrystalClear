9221cb9c8e4b7da02611b3e5b8037c8bf6e0a2ce
Pixxl <oliverhalenius@yahoo.se>
2020-06-07 01:16:39 +0200

    remove redundant comments

--- HierarchySystem/HierarchyManager.cs
+++ HierarchySystem/HierarchyManager.cs
@@ -12,13 +12,12 @@ namespace CrystalClear.HierarchySystem
 		/// <returns>The HierarchyObject at the end of the path</returns>
 		public static HierarchyObject FollowPath(string path)
 		{
-			// Split the path into segments separated by /.
 			string[] pathSegments = path.Split('/');
-			// Store the path to send to the next object to follow.
+
 			string pathToFollow = path;
-			// Store the next object to go to.
+
 			string nextObject = pathSegments[0];
-			// Return the result of following the next object and passing it the path.
+
 			return LoadedHierarchies[nextObject].FollowPath(pathToFollow);
 		}
 
@@ -34,7 +33,6 @@ namespace CrystalClear.HierarchySystem
 		/// <param name="hierarchyObject"></param>
 		public static void AddHierarchy(string name, HierarchyObject hierarchyObject)
 		{
-			// Add the HierarchyObject.
 			LoadedHierarchies.Add(name, hierarchyObject);
 		}
 
@@ -45,11 +43,10 @@ namespace CrystalClear.HierarchySystem
 		/// <param name="newName">The new name to use.</param>
 		public static void SetHierarchyName(HierarchyObject hierarchyObject, string newName)
 		{
-			// Get the current name.
 			string key = GetHierarchyName(hierarchyObject);
-			// Remove the HierarchyObject from LoadedHierarchies using it's old name.
+
 			LoadedHierarchies.Remove(key);
-			// Add the HierarchyObject back with it's new name.
+
 			LoadedHierarchies.Add(newName, hierarchyObject);
 		}
 
@@ -60,11 +57,10 @@ namespace CrystalClear.HierarchySystem
 		/// <param name="newName">The new name of the Hierarchy.</param>
 		public static void SetHierarchyName(string currentName, string newName)
 		{
-			// Get the HierarchyObject that will be renamed.
 			HierarchyObject hierarchy = LoadedHierarchies[currentName];
-			// Remove the Hierarchy from the LoadedHierarchies using it's old name.
+
 			LoadedHierarchies.Remove(currentName);
-			// Add the Hierarchy back with it's new name.
+
 			LoadedHierarchies.Add(newName, hierarchy);
 		}
 
@@ -80,16 +76,11 @@ namespace CrystalClear.HierarchySystem
 				throw new KeyNotFoundException("Hierarchy not found!");
 			}
 
-			// Define a string named key, to be used for storing the key.
 			string key;
 
-			key = LoadedHierarchies // Set key.
-				.First( // Get the first occurance of...
-					x => // X is the KeyValue pair of the dictionary.
-					ReferenceEquals(x.Value, hierarchy)) // If the reference (x's value)'s instance equals the instance of the Hierarchy, then this is the Hierarchy we are searching for.
-							.Key; // Get the item's key.
+			key = LoadedHierarchies.First(x =>ReferenceEquals(x.Value, hierarchy)).Key;
 
-			return key; // Return the key.
+			return key;
 		}
 	}
 }
