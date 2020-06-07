f32dba8170db4518e0bfbf39e1fad76278cdb09b
Pixxl <oliverhalenius@yahoo.se>
2020-06-07 14:17:18 +0200

    add ImaginaryObjectBase

---
+++ SerializationSystem/ImaginaryObjects/ImaginaryObjectBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CrystalClear.SerializationSystem.ImaginaryObjects
+{
+	public abstract class ImaginaryObjectBase<T>
+	{
+		public abstract T CreateInstance();
+
+		internal abstract void WriteConstructionInfo(BinaryWriter writer, Encoding encoding);
+	}
+}
