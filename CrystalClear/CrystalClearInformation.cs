b9520ecc78cb4221ae4f0e8691dde33dba86847d
Pixxl <oliverhalenius@yahoo.se>
2020-06-09 23:17:35 +0200

    use auto property for CrystalClearVersion

--- CrystalClear/CrystalClearInformation.cs
+++ CrystalClear/CrystalClearInformation.cs
@@ -4,8 +4,7 @@ namespace CrystalClear
 {
 	public static class CrystalClearInformation // TODO add a default CrystalClearException for all other to derive from.
 	{
-		private static readonly Version crystalClearVersion = new Version(0, 0, 0, 5);
-		public static Version CrystalClearVersion { get => crystalClearVersion; }
+		public static Version CrystalClearVersion { get; } = new Version(0, 0, 0, 5);
 
 		public static string WorkingPath => $@"{Environment.CurrentDirectory}\"; // TODO make constant, also maybe rename to WorkPath?
 
