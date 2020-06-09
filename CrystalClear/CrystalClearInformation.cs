32e038e142994b6fbcefe855cff19a010c4e1cec
Pixxl <oliverhalenius@yahoo.se>
2020-06-09 23:16:11 +0200

    version bump as future changes are going to make older serialized data unusable in new version

--- CrystalClear/CrystalClearInformation.cs
+++ CrystalClear/CrystalClearInformation.cs
@@ -4,7 +4,7 @@ namespace CrystalClear
 {
 	public static class CrystalClearInformation // TODO add a default CrystalClearException for all other to derive from.
 	{
-		private static readonly Version crystalClearVersion = new Version(0, 0, 0, 4);
+		private static readonly Version crystalClearVersion = new Version(0, 0, 0, 5);
 		public static Version CrystalClearVersion { get => crystalClearVersion; }
 
 		public static string WorkingPath => $@"{Environment.CurrentDirectory}\"; // TODO make constant, also maybe rename to WorkPath?
