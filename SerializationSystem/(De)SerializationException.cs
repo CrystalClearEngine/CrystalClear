11d14cb58acc8e394cb2a0615be5b2b3e72b7496
Pixxl <oliverhalenius@yahoo.se>
2020-06-18 19:20:24 +0200

    add SerializationException classes

---
+++ SerializationSystem/(De)SerializationException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CrystalClear.SerializationSystem
+{
+	public class SerializationException : Exception
+	{
+		public readonly long ApproxPositionOfError;
+
+		public SerializationException(BinaryWriter writer, string message) : this(writer, message, null)
+		{
+		}
+
+		public SerializationException(BinaryWriter writer, string message, Exception innerException) : base(message + " Encountered at (approximately) position:" + writer.BaseStream.Position, innerException)
+		{
+			ApproxPositionOfError = writer.BaseStream.Position;
+		}
+	}
+
+	public class DeserializationException : Exception
+	{
+		public readonly long ApproxPositionOfError;
+
+		public DeserializationException(BinaryReader reader, string message) : this(reader, message, null)
+		{
+		}
+
+		public DeserializationException(BinaryReader reader, string message, Exception innerException) : base(message + " Encountered at (approximately) position:" + reader.BaseStream.Position, innerException)
+		{
+			ApproxPositionOfError = reader.BaseStream.Position;
+		}
+	}
+}
