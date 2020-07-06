95e9baa0f1de324ed577bcbe1b1a8b0a241920c1
Pixxl <oliverhalenius@yahoo.se>
2020-07-06 15:04:22 +0200

    disallow multiple instances of the OnRecieveMessageAttribute

--- MessageSystem/OnReceiveMessageAttribute.cs
+++ MessageSystem/OnReceiveMessageAttribute.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CrystalClear.MessageSystem
 {
-	[AttributeUsage(AttributeTargets.Method)]
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 	public class OnReceiveMessageAttribute : Attribute
 	{
 		public Type MessageType;
