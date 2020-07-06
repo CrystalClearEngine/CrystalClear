be0c7c2a130d9d7cc212b17ee084bebc0dd69ae3
Pixxl <oliverhalenius@yahoo.se>
2020-07-06 16:04:31 +0200

    add Messager to Scripts

---
+++ Scripts/Messager.cs
@@ -0,0 +1,43 @@
+﻿using CrystalClear;
+using CrystalClear.EventSystem.StandardEvents;
+using CrystalClear.MessageSystem;
+using CrystalClear.ScriptUtilities;
+using System;
+
+namespace Scripts
+{
+	[IsScript]
+	class Messager
+	{
+		[OnStartEvent]
+		void SendMessage()
+		{
+			Recipient recipient = new Recipient();
+
+			MyMessage myMessage = new MyMessage()
+			{
+				Data = "MyData"
+			};
+
+			myMessage.Send(recipient);
+		}
+	}
+
+	class Recipient
+	{
+		[OnReceiveMessage(typeof(MyMessage))]
+		void RecieveMyMessage(MyMessage myMessage)
+		{
+			Output.Log("Recieved MyMessage!");
+			Output.Log("Data: " + myMessage.Data);
+		}
+	}
+
+	class MyMessage : Message
+	{
+		public string Data;
+
+		delegate void MyMessageDelegate(MyMessage message);
+		public override Type DelegateType => typeof(MyMessageDelegate);
+	}
+}
