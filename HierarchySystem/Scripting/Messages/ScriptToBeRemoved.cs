40a9471e97b14cc06d04623171a822d92431a18d
Pixxl <oliverhalenius@yahoo.se>
2020-07-06 18:06:28 +0200

    add ScriptToBeRemoved

---
+++ HierarchySystem/Scripting/Messages/ScriptToBeRemoved.cs
@@ -0,0 +1,17 @@
+﻿using CrystalClear.MessageSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrystalClear.HierarchySystem.Scripting.Messages
+{
+	/// <summary>
+	/// This message will be sent to any script that is going to be removed.
+	/// </summary>
+	public class ScriptToBeRemoved : Message
+	{
+		public delegate void ScriptToBeRemovedDelegate(ScriptToBeRemoved message);
+
+		public override Type DelegateType => typeof(ScriptToBeRemovedDelegate);
+	}
+}
