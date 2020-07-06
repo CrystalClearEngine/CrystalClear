c93fb233a2b89bfa2a186ddb45cfbb889b44748a
Pixxl <oliverhalenius@yahoo.se>
2020-07-06 18:09:31 +0200

    add HierarchyObjectToBeDeleted

---
+++ HierarchySystem/Messages/HierarchyObjectToBeDeleted.cs
@@ -0,0 +1,17 @@
+﻿using CrystalClear.MessageSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrystalClear.HierarchySystem.Messages
+{
+	/// <summary>
+	/// This message will be sent to any HierarchyObject that is going to be removed.
+	/// </summary>
+	public class HierarchyObjectToBeDeleted : Message
+	{
+		public delegate void ScriptToBeRemovedDelegate(HierarchyObjectToBeDeleted message);
+
+		public override Type DelegateType => typeof(ScriptToBeRemovedDelegate);
+	}
+}
