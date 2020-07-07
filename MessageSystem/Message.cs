387901a894af2be09bff247c2b988d75395aab52
Pixxl <oliverhalenius@yahoo.se>
2020-07-07 21:16:54 +0200

    add AllowStaticMethods and AllowInstanceMethods virtual properties to Message

--- MessageSystem/Message.cs
+++ MessageSystem/Message.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
 
 namespace CrystalClear.MessageSystem
 {
 	public abstract class Message
 	{
+		// TODO: use flag enum instead.
+		public virtual bool AllowStaticMethods => true;
+
+		public virtual bool AllowInstanceMethods => true;
+
 		public abstract Type DelegateType { get; }
 
 		public void SendTo(object recipient)
--- MessageSystem/MessageSystem.cs
+++ MessageSystem/MessageSystem.cs
@@ -12,6 +12,9 @@ namespace CrystalClear.MessageSystem
 
 		public static void SendMessage(this object recipient, Message message)
 		{
+			if (!message.AllowInstanceMethods)
+				return;
+
 			var messageRecievers = (from MethodInfo method in recipient.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) where method.GetCustomAttribute<OnReceiveMessageAttribute>()?.MessageType == message.GetType() select method);
 
 			var toCall = (from MethodInfo method in messageRecievers select method.CreateDelegate(message.DelegateType, recipient));
@@ -23,8 +26,16 @@ namespace CrystalClear.MessageSystem
 			}
 		}
 
+		/// <summary>
+		/// Sends a message to all static message recipient methods in the type.
+		/// </summary>
+		/// <param name="recipient">The recipient type that should receive the message.</param>
+		/// <param name="message">The actual message to send.</param>
 		public static void SendMessage(this Type recipient, Message message)
 		{
+			if (!message.AllowStaticMethods)
+				return;
+
 			var messageRecievers = (from MethodInfo method in recipient.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic) where method.GetCustomAttribute<OnReceiveMessageAttribute>()?.MessageType == message.GetType() select method);
 
 			var toCall = (from MethodInfo method in messageRecievers select method.CreateDelegate(message.DelegateType));
