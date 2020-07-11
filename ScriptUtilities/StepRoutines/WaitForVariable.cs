822c44540494354ab765ce0d40e715f0293b8010
Pixxl <oliverhalenius@yahoo.se>
2020-07-11 23:59:10 +0200

    comment out class, as it should not even have been commited at all...

--- ScriptUtilities/StepRoutines/WaitForVariable.cs
+++ ScriptUtilities/StepRoutines/WaitForVariable.cs
@@ -2,13 +2,13 @@
 
 namespace CrystalClear.ScriptUtilities.StepRoutines
 {
-	public class WaitForVariable : WaitFor
-	{
+	//public class WaitForVariable : WaitFor
+	//{
 
 
-		public WaitForVariable(ref object toCheck, object toWaitFor, ScriptEventBase checkOn)
-		{
+	//	public WaitForVariable(ref object toCheck, object toWaitFor, ScriptEventBase checkOn)
+	//	{
 
-		}
-	}
+	//	}
+	//}
 }
