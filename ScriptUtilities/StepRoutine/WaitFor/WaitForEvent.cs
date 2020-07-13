f338ef006c15beb8d571545569d5da32bd4f66ff
Pixxl <oliverhalenius@yahoo.se>
2020-07-13 15:04:11 +0200

    implement Start()

--- ScriptUtilities/StepRoutine/WaitFor/WaitForEvent.cs
+++ ScriptUtilities/StepRoutine/WaitFor/WaitForEvent.cs
@@ -11,6 +11,8 @@ namespace CrystalClear.ScriptUtilities.StepRoutines
 	{
 		ScriptEventBase EventToWaitFor;
 
+		Action proceeder; // A delegate that will simply call ProceedStepRoutine. It is here so that Cancel and Cleanup can access it.
+
 		public WaitForEvent(Type scriptEventType)
 		{
 			EventToWaitFor =
@@ -22,6 +24,35 @@ namespace CrystalClear.ScriptUtilities.StepRoutines
 		public WaitForEvent(ScriptEventBase scriptEvent)
 		{
 			EventToWaitFor = scriptEvent;
+		}
+
+		public override void Start(StepRoutineInfo stepRoutine)
+		{
+			/* To achieve the effect we want, we should create a delegate that calls ProceedStepRoutine.
+			 * We subscribe that delegate to the event.
+			 * When the event is raised ProceedStepRoutine will be called, then Cleanup will be called
+			 * from there and the StepRoutine will proceed.
+			*/
+
+			// Create the proceeder delegate.
+			proceeder = new Action( // TODO: make utility method that generates this exact delegate, or perhaps even have it as a property on WaitFor?
+				delegate
+				{
+					StepRoutine.ProceedStepRoutine(stepRoutine);
+				});
+
+			// Subscribe the delegate to the event so it will be called when the event is raised.
+			EventToWaitFor.Subscribe(proceeder);
+		}
+
+		public override void Cancel()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal override void Cleanup()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
