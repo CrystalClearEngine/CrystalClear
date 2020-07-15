b6a9c976ba2b46acc2c15ae8c14a35350f7b75f7
Pixxl <oliverhalenius@yahoo.se>
2020-07-15 13:34:17 +0200

    add CustomAssemblyLoadContext

---
+++ EditorMain/CustomAssemblyLoadContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Loader;
+using System.Text;
+
+namespace EditorMain
+{
+	public class CustomAssemblyLoadContext : AssemblyLoadContext
+	{
+		public CustomAssemblyLoadContext() : base(isCollectible: true)
+		{
+
+		}
+	}
+}
