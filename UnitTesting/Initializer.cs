c09c51efb512bae62f1a4f7b95762cd43a59b8df
Pixxl <oliverhalenius@yahoo.se>
2020-07-16 14:08:04 +0200

    add Initializer and run cleanup

---
+++ UnitTesting/Initializer.cs
@@ -0,0 +1,23 @@
+﻿using CrystalClear;
+using CrystalClear.HierarchySystem;
+using CrystalClear.Standard.HierarchyObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class Initializer
+	{
+		[AssemblyInitialize]
+		public static void Init(TestContext testContext)
+		{
+			CrystalClearInformation.UserAssemblies = new[]
+			{
+				Assembly.GetExecutingAssembly(),
+				Assembly.GetAssembly(typeof(ScriptObject)),
+				Assembly.GetAssembly(typeof(HierarchyObject)),
+			};
+		}
+	}
+}
