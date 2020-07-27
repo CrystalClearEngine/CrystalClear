d56b67cde89952178126073ef7cc3a108bd75d5c
Pixxl <oliverhalenius@yahoo.se>
2020-07-27 13:54:41 +0200

    split MainClass into multiple files

---
+++ EditorMain/MainProjectSelect.cs
@@ -0,0 +1,35 @@
+﻿using CrystalClear;
+using System;
+using static CrystalClear.Input;
+
+partial class MainClass
+{
+	private static void ProjectSelect()
+	{
+		#region Project Selection
+		Output.Log("Please open or create a new project:");
+		ProjectSelection:
+		switch (Console.ReadLine())
+		{
+			case "new":
+				ProjectInfo.NewProject(AskQuestion("Pick a path for the new project"), AskQuestion("Pick a name for the new project"));
+				break;
+
+			case "open":
+				try
+				{
+					ProjectInfo.OpenProject(AskQuestion("Enter the path of the project"));
+				}
+				catch (ArgumentException)
+				{
+					goto ProjectSelection;
+				}
+				break;
+
+			default:
+				Output.ErrorLog("command error: unknown command");
+				goto ProjectSelection;
+		}
+		#endregion
+	}
+}
---
+++ EditorMain/MainRun.cs
@@ -0,0 +1,36 @@
+﻿using CrystalClear;
+using CrystalClear.SerializationSystem.ImaginaryObjects;
+using System;
+using System.Diagnostics;
+using static CrystalClear.EditorInformation;
+
+partial class MainClass
+{
+	private static void Run(ImaginaryHierarchyObject rootHierarchyObject)
+	{
+		#region Running
+		Console.Write("Choose a name for the hierarchy: ");
+		string hierarchyName = Console.ReadLine();
+
+		Output.Log();
+
+		Process userProcess = new Process();
+
+		userProcess.StartInfo = new ProcessStartInfo(@"E:\dev\crystal clear\RuntimeMain\bin\Debug\netcoreapp3.1\RuntimeMain.exe", CurrentProject.BuildPath + @"\UserGeneratedCode.dll");
+
+		userProcess.Start();
+
+		userProcess.WaitForExit();
+
+		Console.WriteLine($"The program exited with code {userProcess.ExitCode}.");
+
+		userProcess.Dispose();
+
+		Console.WriteLine("Press any key to continue.");
+
+		Console.ReadKey(true);
+
+		Console.Clear();
+		#endregion
+	}
+}
