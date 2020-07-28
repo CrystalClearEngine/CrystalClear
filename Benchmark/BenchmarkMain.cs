3f8a09330b34c617708011215cbe7bc151455c64
Pixxl <oliverhalenius@yahoo.se>
2020-07-28 16:59:38 +0200

    add benchmarks

---
+++ Benchmark/BenchmarkMain.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benchmarks
+{
+	public static class BenchmarkMain
+	{
+		static readonly Type[] benchmarks =
+		{
+			typeof(SubscriptionOrderedVNonOrdered),
+
+		};
+
+		public static void Main()
+		{
+			Console.WriteLine("Running all benchmarks!");
+
+			foreach (var benchmark in benchmarks)
+			{
+				BenchmarkRunner.Run(benchmark, new DebugBuildConfig());
+			}
+		}
+	}
+}
---
+++ Benchmark/Benchmarks/EventSystem/SubscriptionOrderedVNonOrdered.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using CrystalClear.EventSystem;
+using CrystalClear.EventSystem.StandardEvents;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Reflection;
+
+namespace Benchmarks
+{
+	public class SubscriptionOrderedVNonOrdered
+	{
+		[OnStartEvent]
+		public void OnStart()
+		{ }
+
+		[Benchmark]
+		public void OrderedSubscription()
+		{
+			EventSystem.SubscribeEvents(GetType(), this);
+		}
+
+		[Benchmark]
+		public void UnorderedSubscription()
+		{
+			SubscribeEventsUnordered(GetType(), this);
+		}
+
+		public static void SubscribeEventsUnordered(Type typeToSubscribe, object instance)
+		{
+			foreach (MethodInfo method in typeToSubscribe.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+			{
+				SubscribeToAttribute subscribeToAttribute = method.GetCustomAttribute<SubscribeToAttribute>();
+				subscribeToAttribute?.ScriptEvent.Subscribe(method, instance);
+			}
+		}
+	}
+}
