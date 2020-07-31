41ecd86171c70428232dd6b4065200bdc40890d9
Pixxl <oliverhalenius@yahoo.se>
2020-07-31 18:37:48 +0200

    trying out pipes

--- EditorMain/MainRun.cs
+++ EditorMain/MainRun.cs
@@ -4,7 +4,9 @@ using CrystalClear.RuntimeMain;
 using CrystalClear.SerializationSystem.ImaginaryObjects;
 using System;
 using System.Diagnostics;
+using System.IO.Pipes;
 using System.Reflection;
+using System.Text;
 using static CrystalClear.EditorInformation;
 
 partial class MainClass
@@ -28,18 +30,27 @@ partial class MainClass
 
 		RuntimeMain.RunWithImaginaryHierarchyObject(new[] { compiledAssembly }, hierarchyName, rootHierarchyObject);
 
-		while (true) ; // In DEBUG mode you cannot exit the run.
+		while (true) ; // In DEBUG mode you cannot exit the run, since it runs it in the engine.
 #endif
 
-#if RELEASE // TODO: send and recieve path to Hierarchy to use and also use anonymous pipes for communication.
+#if RELEASE
 		Process userProcess = new Process();
 
-		userProcess.StartInfo = new ProcessStartInfo(@"E:\dev\crystal clear\RuntimeMain\bin\Debug\netcoreapp3.1\RuntimeMain.exe", CurrentProject.BuildPath + @"\UserGeneratedCode.dll");
+		NamedPipeServerStream debugStream = new NamedPipeServerStream("CrystalClearDebugStream", PipeDirection.InOut);
+
+		string args = $"{CurrentProject.BuildPath + @"\UserGeneratedCode.dll"} {"CrystalClearDebugStream"} --debug";
+
+		userProcess.StartInfo = new ProcessStartInfo(@"E:\dev\crystal clear\RuntimeMain\bin\Debug\netcoreapp3.1\RuntimeMain.exe", args);
 
 		userProcess.Start();
 
+		debugStream.WaitForConnection();
+
+		WriteString(debugStream, "Hello!");
+
 		userProcess.WaitForExit();
 
+		#region Exiting
 		Console.WriteLine($"The program exited with code {userProcess.ExitCode}.");
 
 		userProcess.Dispose();
@@ -49,7 +60,36 @@ partial class MainClass
 		Console.ReadKey(true);
 
 		Console.Clear();
+		#endregion
 #endif
-#endregion
+		#endregion
+	}
+
+	public static string ReadString(PipeStream ioStream)
+	{
+		int len = 0;
+
+		len = ioStream.ReadByte() * 256;
+		len += ioStream.ReadByte();
+		byte[] inBuffer = new byte[len];
+		ioStream.Read(inBuffer, 0, len);
+
+		return Encoding.ASCII.GetString(inBuffer);
+	}
+
+	public static int WriteString(PipeStream ioStream, string outString)
+	{
+		byte[] outBuffer = Encoding.ASCII.GetBytes(outString);
+		int len = outBuffer.Length;
+		if (len > UInt16.MaxValue)
+		{
+			len = (int)UInt16.MaxValue;
+		}
+		ioStream.WriteByte((byte)(len / 256));
+		ioStream.WriteByte((byte)(len & 255));
+		ioStream.Write(outBuffer, 0, len);
+		ioStream.Flush();
+
+		return outBuffer.Length + 2;
 	}
 }
--- RuntimeMain/RuntimeMain.cs
+++ RuntimeMain/RuntimeMain.cs
@@ -4,7 +4,11 @@ using CrystalClear.SerializationSystem.ImaginaryObjects;
 using CrystalClear.Standard.HierarchyObjects;
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 using static CrystalClear.ScriptUtilities.Utilities.ConsoleInput;
 
 namespace CrystalClear.RuntimeMain
@@ -14,6 +18,23 @@ namespace CrystalClear.RuntimeMain
 		public static void Main(string[] args)
 		{
 			string assemblyPath = string.IsNullOrEmpty(args[0]) ? AskQuestion("Please enter a UserGeneratedCode to use.") : args[0];
+
+			bool isDebug = args.Contains("--debug");
+
+			NamedPipeClientStream debugStream;
+
+			if (isDebug)
+			{
+				Debugger.Launch();
+
+				string pipeName = args[1];
+
+				debugStream = new NamedPipeClientStream(pipeName);
+
+				debugStream.Connect();
+
+				Console.WriteLine(ReadString(debugStream));
+			}
 
 			Assembly compiledAssembly = Assembly.LoadFrom(assemblyPath);
 
@@ -37,6 +58,34 @@ namespace CrystalClear.RuntimeMain
 			}
 			goto ExitHandling;
 			#endregion
+
+			static string ReadString(PipeStream ioStream)
+			{
+				int len = 0;
+
+				len = ioStream.ReadByte() * 256;
+				len += ioStream.ReadByte();
+				byte[] inBuffer = new byte[len];
+				ioStream.Read(inBuffer, 0, len);
+
+				return Encoding.ASCII.GetString(inBuffer);
+			}
+
+			static int WriteString(PipeStream ioStream, string outString)
+			{
+				byte[] outBuffer = Encoding.ASCII.GetBytes(outString);
+				int len = outBuffer.Length;
+				if (len > UInt16.MaxValue)
+				{
+					len = (int)UInt16.MaxValue;
+				}
+				ioStream.WriteByte((byte)(len / 256));
+				ioStream.WriteByte((byte)(len & 255));
+				ioStream.Write(outBuffer, 0, len);
+				ioStream.Flush();
+
+				return outBuffer.Length + 2;
+			}
 		}
 
 		public static bool IsRunning = false;
