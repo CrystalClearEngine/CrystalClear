38e0add31d1a21636257c56b7660d1c5c23b5188
Pixxl <oliverhalenius@yahoo.se>
2020-08-02 13:26:14 +0200

    remove the process running

--- EditorMain/MainRun.cs
+++ EditorMain/MainRun.cs
@@ -2,12 +2,9 @@
 using CrystalClear.HierarchySystem;
 using CrystalClear.RuntimeMain;
 using CrystalClear.SerializationSystem.ImaginaryObjects;
+using CrystalClear.Standard.HierarchyObjects;
 using System;
-using System.Diagnostics;
-using System.IO.Pipes;
 using System.Reflection;
-using System.Text;
-using static CrystalClear.EditorInformation;
 
 partial class MainClass
 {
@@ -16,7 +13,7 @@ partial class MainClass
 		#region Running
 		if (compiledAssembly is null)
 		{
-			Output.ErrorLog("error: code not compiling");
+			Output.ErrorLog("error: cannot run if code did not compile");
 			return;
 		}
 
@@ -25,71 +22,19 @@ partial class MainClass
 
 		Output.Log();
 
-#if DEBUG
-		CrystalClearInformation.UserAssemblies = new[] { compiledAssembly, Assembly.GetAssembly(typeof(HierarchyObject)) };
+		RuntimeInformation.UserAssemblies = new[] { compiledAssembly, Assembly.GetAssembly(typeof(HierarchyObject)), Assembly.GetAssembly(typeof(ScriptObject)) };
 
 		RuntimeMain.RunWithImaginaryHierarchyObject(new[] { compiledAssembly }, hierarchyName, rootHierarchyObject);
 
-		while (true) ; // In DEBUG mode you cannot exit the run, since it runs it in the engine.
-#endif
-
-#if RELEASE
-		Process userProcess = new Process();
-
-		NamedPipeServerStream debugStream = new NamedPipeServerStream("CrystalClearDebugStream", PipeDirection.InOut);
-
-		string args = $"{CurrentProject.BuildPath + @"\UserGeneratedCode.dll"} {"CrystalClearDebugStream"} --debug";
-
-		userProcess.StartInfo = new ProcessStartInfo(@"E:\dev\crystal clear\RuntimeMain\bin\Debug\netcoreapp3.1\RuntimeMain.exe", args);
-
-		userProcess.Start();
-
-		debugStream.WaitForConnection();
-
-		WriteString(debugStream, "Hello!");
-
-		userProcess.WaitForExit();
-
-		#region Exiting
-		Console.WriteLine($"The program exited with code {userProcess.ExitCode}.");
-
-		userProcess.Dispose();
-
-		Console.WriteLine("Press any key to continue.");
-
-		Console.ReadKey(true);
-
-		Console.Clear();
-		#endregion
-#endif
-		#endregion
-	}
-
-	public static string ReadString(PipeStream ioStream)
-	{
-		int len = 0;
-
-		len = ioStream.ReadByte() * 256;
-		len += ioStream.ReadByte();
-		byte[] inBuffer = new byte[len];
-		ioStream.Read(inBuffer, 0, len);
-
-		return Encoding.ASCII.GetString(inBuffer);
-	}
-
-	public static int WriteString(PipeStream ioStream, string outString)
-	{
-		byte[] outBuffer = Encoding.ASCII.GetBytes(outString);
-		int len = outBuffer.Length;
-		if (len > UInt16.MaxValue)
+		while (RuntimeMain.IsRunning)
 		{
-			len = (int)UInt16.MaxValue;
+			if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+			{
+				RuntimeMain.Stop();
+			}
 		}
-		ioStream.WriteByte((byte)(len / 256));
-		ioStream.WriteByte((byte)(len & 255));
-		ioStream.Write(outBuffer, 0, len);
-		ioStream.Flush();
 
-		return outBuffer.Length + 2;
+		RuntimeInformation.UserAssemblies = null;
+		#endregion
 	}
 }
