e71822081013b90abf625cea6ca862bed37d823a
Pixxl <oliverhalenius@yahoo.se>
2020-08-03 19:55:08 +0200

    implement INotifyPropertyChanged, remove generic version, and rename okBooler to success and update docs

--- HierarchySystem/Hierarchy.cs
+++ HierarchySystem/Hierarchy.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using CrystalClear.HierarchySystem.Annotations;
 
 namespace CrystalClear.HierarchySystem
 {
-	public class Hierarchy
+	public sealed class Hierarchy
 		: IDictionary<string, HierarchyObject>
 		, IEquatable<Hierarchy>
-	{ // TODO maybe implement IPropertyChanged?
+		, INotifyPropertyChanged
+	{
 		public Hierarchy(HierarchyObject owner)
 		{
+			OnHierarchyChange += () => OnPropertyChanged();
 			OnHierarchyChange += owner.OnLocalHierarchyChange;
 		}
 
@@ -27,7 +32,7 @@ namespace CrystalClear.HierarchySystem
 		}
 
 		/// <summary>
-		/// This event is called when a change in the Hierarchy has been made, such as changing the value of a child, or adding or removing a child.
+		/// This event is raised when a change in the Hierarchy has been made, such as changing the value of a child, or adding or removing a child.
 		/// </summary>
 		public event Action OnHierarchyChange;
 
@@ -104,9 +109,9 @@ namespace CrystalClear.HierarchySystem
 		/// <param name="childName">The child's name.</param>
 		public bool RemoveChild(string childName)
 		{
-			bool okBooler = hierarchy.Remove(childName);
+			bool success = hierarchy.Remove(childName);
 			OnHierarchyChange();
-			return okBooler;
+			return success;
 		}
 
 		#region Dictionary Implementation
@@ -167,169 +172,13 @@ namespace CrystalClear.HierarchySystem
 		{
 			return !(left == right);
 		}
-	}
 
-	public struct Hierarchy<T>
-		: IDictionary<string, T>
-		, IEquatable<Hierarchy<T>>
-	{
-		public Hierarchy(Dictionary<string, T> baseDictionary = null)
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		private void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
-			hierarchy = baseDictionary is null ? new Dictionary<string, T>() : baseDictionary;
-			OnHierarchyChange = null;
-		}
-
-		private Dictionary<string, T> hierarchy;
-
-		public T this[string index]
-		{
-			get => hierarchy[index];
-			set
-			{
-				hierarchy[index] = value;
-				OnHierarchyChange();
-			}
-		}
-
-		/// <summary>
-		/// This event is called when a change in the Hierarchy has been made, such as changing the value of a child, or adding or removing a child.
-		/// </summary>
-		public event Action OnHierarchyChange;
-
-		#region Properties
-		public ICollection<string> Keys => hierarchy.Keys;
-
-		public ICollection<T> Values => hierarchy.Values;
-
-		public int Count => hierarchy.Count;
-
-		public bool IsReadOnly => false;
-		#endregion
-
-		#region Management
-		/// <summary>
-		/// This method sets the name of the specified child to the specified new key. 
-		/// </summary>
-		/// <param name="child">The T that should receive a name change</param>
-		/// <param name="newName">The new name for the child</param>
-		public void SetChildName(T child, string newName)
-		{
-			string key = GetChildName(child);
-			RemoveChild(key);
-			AddChild(newName, child);
-		}
-
-		/// <summary>
-		/// This method sets the name of the specified key in the Hierarchy to the new key. 
-		/// </summary>
-		/// <param name="currentName">The T that should receive a name change</param>
-		/// <param name="newName">The new name for the child</param>
-		public void SetChildName(string currentName, string newName)
-		{
-			T T = hierarchy[currentName];
-			RemoveChild(currentName);
-			AddChild(newName, T);
-		}
-
-		/// <summary>
-		/// Returns the name of a child T that is present in the Hierarchy of this T.
-		/// </summary>
-		/// <param name="child">The T to get the name of.</param>
-		/// <returns>The name of this object.</returns>
-		public string GetChildName(T child)
-		{
-			string key = hierarchy.First(x => ReferenceEquals(x.Value, child)).Key;
-			return key;
-		}
-
-		/// <summary>
-		/// Adds a T to the Hierarchy and also set the T's parent accordingly
-		/// </summary>
-		/// <param name="name">The name of the T to add.</param>
-		/// <param name="child">The T to add.</param>
-		public void AddChild(string name, T child)
-		{
-			hierarchy.Add(name, child);
-			OnHierarchyChange?.Invoke();
-		}
-
-		/// <summary>
-		/// Removes the specified child specified by T from the Hierarchy.
-		/// </summary>
-		/// <param name="child">The child T to remove.</param>
-		public void RemoveChild(T child)
-		{
-			string key = hierarchy.First(T => T.Value.Equals(child)).Key;
-			RemoveChild(key);
-		}
-
-		/// <summary>
-		/// Removes the specified child specified by name from the Hierarchy.
-		/// </summary>
-		/// <param name="childName">The child's name.</param>
-		public bool RemoveChild(string childName)
-		{
-			bool okBooler = hierarchy.Remove(childName);
-			OnHierarchyChange();
-			return okBooler;
-		}
-
-		#region Dictionary Implementation
-		public bool ContainsKey(string key) => hierarchy.ContainsKey(key);
-
-		public void Add(string key, T value) => AddChild(key, value);
-
-		public bool Remove(string key) => RemoveChild(key);
-
-		public bool TryGetValue(string key, out T value) => hierarchy.TryGetValue(key, out value);
-
-		public void Add(KeyValuePair<string, T> item) => AddChild(item.Key, item.Value);
-
-		public void Clear() => hierarchy.Clear();
-
-		public bool Contains(KeyValuePair<string, T> item) => hierarchy.Contains(item);
-
-		void ICollection<KeyValuePair<string, T>>.CopyTo(KeyValuePair<string, T>[] array, int arrayIndex) => throw new NotSupportedException();
-
-		public bool Remove(KeyValuePair<string, T> item) => RemoveChild(item.Key);
-
-		public IEnumerator<KeyValuePair<string, T>> GetEnumerator() => hierarchy.GetEnumerator();
-
-		IEnumerator IEnumerable.GetEnumerator() => hierarchy.GetEnumerator();
-		#endregion
-		#endregion
-
-		public override bool Equals(object obj)
-		{
-			return Equals(obj as Hierarchy);
-
-			//if (!(obj is Hierarchy))
-			//	return false;
-
-			//Hierarchy hierarchy = (Hierarchy)obj;
-
-			//return (this.hierarchy.Equals(hierarchy.hierarchy) &
-			//	this.OnHierarchyChange.Equals(hierarchy.OnHierarchyChange));
-		}
-
-		public bool Equals(Hierarchy<T> other)
-		{
-			return hierarchy.Equals(other.hierarchy) || hierarchy.Count == 0 && other.hierarchy.Count == 0;
-		}
-
-		public override int GetHashCode()
-		{
-			return 218564712 + EqualityComparer<Dictionary<string, T>>.Default.GetHashCode(hierarchy);
-		}
-
-		public static bool operator ==(Hierarchy<T> left, Hierarchy<T> right)
-		{
-			return EqualityComparer<Hierarchy<T>>.Default.Equals(left, right);
-		}
-
-		public static bool operator !=(Hierarchy<T> left, Hierarchy<T> right)
-		{
-			return !(left == right);
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 	}
 }
