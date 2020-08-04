020e9d0fb2951ba91eaa0d4f6137ec5107394590
Pixxl <oliverhalenius@yahoo.se>
2020-08-05 00:22:21 +0200

    update README and bump version

--- CrystalClear/CrystalClearInformation.cs
+++ CrystalClear/CrystalClearInformation.cs
@@ -5,7 +5,7 @@ namespace CrystalClear
 {
 	public static class CrystalClearInformation // TODO add a default CrystalClearException for all other to derive from.
 	{
-		public static Version CrystalClearVersion { get; } = new Version(0, 0, 0, 5);
+		public static Version CrystalClearVersion { get; } = new Version(0, 0, 0, 6);
 
 		public static string WorkingPath => $@"{Environment.CurrentDirectory}\"; // TODO make constant, also maybe rename to WorkPath?
 
