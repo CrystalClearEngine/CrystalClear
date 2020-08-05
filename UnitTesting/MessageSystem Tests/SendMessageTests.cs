e2319ff99246a88136c579849945206c4c15b016
Pixxl <oliverhalenius@yahoo.se>
2020-08-06 00:00:42 +0200

    add StaticSendMessageTest

--- UnitTesting/MessageSystem Tests/SendMessageTests.cs
+++ UnitTesting/MessageSystem Tests/SendMessageTests.cs
@@ -1,6 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System.Data;
+using System.Diagnostics;
 using CrystalClear.MessageSystem;
+using Microsoft.Scripting.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using Message = CrystalClear.MessageSystem.Message;
 
 namespace UnitTests
@@ -56,6 +59,56 @@ namespace UnitTests
 
 			Debug.WriteLine(recipient.ReceivedMessages);
 			Assert.IsTrue(recipient.ReceivedMessages == 4);
+		}
+
+		[TestMethod]
+		public void StaticSendMessageTest()
+		{
+			var recipient = typeof(StaticMessageRecipient);
+			
+			new TestMessage().SendTo(recipient);
+
+			Debug.WriteLine(StaticMessageRecipient.ReceivedMessages);
+			Assert.IsTrue(StaticMessageRecipient.ReceivedMessages == 4);
+		}
+	}
+
+	public static class StaticMessageRecipient
+	{
+		public static int ReceivedMessages = 0;
+
+		[OnReceiveMessage(typeof(TestMessage))]
+		public static void TestMessageReceiver()
+		{
+			ReceivedMessages++;
+		}
+
+		[OnReceiveMessage(typeof(TestMessage))]
+		public static void AnotherTestMessageReceiver(Message message)
+		{
+			ReceivedMessages++;
+		}
+
+		[OnReceiveMessage(typeof(TestMessage))]
+		public static void YetAnotherTestMessageReceiver(TestMessage message)
+		{
+			ReceivedMessages++;
+		}
+
+		[OnReceiveMessage(typeof(Message))]
+		public static void YetYetAnotherTestMessageReceiver(Message message)
+		{
+			ReceivedMessages++;
+
+			Assert.IsInstanceOfType(message, typeof(TestMessage));
+		}
+
+		[OnReceiveMessage(typeof(Message), ReceiveSubclasses = false)]
+		public static void YetYetYetAnotherTestMessageReceiver(Message message)
+		{
+			ReceivedMessages++;
+
+			Assert.Fail("This means that a subclass was received even though it was not supposed to be.");
 		}
 	}
 }
