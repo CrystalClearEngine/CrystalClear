94d11c1dd7b85cbe28984a1b869b4e2768ef93e1
Pixxl <oliverhalenius@yahoo.se>
2020-08-06 00:28:05 +0200

    add benchmarks for Vector surprised by the not terrible results!

---
+++ Benchmarking/Benchmarks/Vector/VectorCreateBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using BenchmarkDotNet.Attributes;
+using CrystalClear.ScriptUtilities;
+
+namespace Benchmarks
+{
+	[MemoryDiagnoser]
+	public class VectorCreateBenchmark
+	{
+		[Benchmark(Baseline = true)]
+		public Vector3 Vector3Create()
+		{
+			return new Vector3();
+		}
+		
+		[Benchmark]
+		public CrystalClear.ScriptUtilities.Vector VectorCreate()
+		{
+			return new CrystalClear.ScriptUtilities.Vector(3);
+		}
+	}
+}
---
+++ Benchmarking/Benchmarks/Vector/VectorMathsBenchmark.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using BenchmarkDotNet.Attributes;
+using CrystalClear.ScriptUtilities;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+
+namespace Benchmarks
+{
+	[MemoryDiagnoser]
+	public class VectorMathsBenchmark
+	{
+		[Benchmark(Baseline = true)]
+		public Vector3 Vector3Maths()
+		{
+			var vector3 = new Vector3();
+			
+			return vector3 += new Vector3(3, 1, 3);
+		}
+		
+		[Benchmark]
+		public CrystalClear.ScriptUtilities.Vector VectorMaths()
+		{
+			var vector = new CrystalClear.ScriptUtilities.Vector(3);
+			
+			return vector += new CrystalClear.ScriptUtilities.Vector(3, 1, 3);
+		}
+	}
+}
