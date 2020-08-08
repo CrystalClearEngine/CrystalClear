6e54e93850800f7281ba1426db49e69cba5d0177
Pixxl <oliverhalenius@yahoo.se>
2020-08-08 12:25:00 +0200

    WIP on analyzer

--- CrystalClearAnalyzer/CrystalClearAnalyzer/MethodParameterMessageTypeMisMatchAnalyzer.cs
+++ CrystalClearAnalyzer/CrystalClearAnalyzer/MethodParameterMessageTypeMisMatchAnalyzer.cs
@@ -30,21 +30,25 @@ namespace CrystalClearAnalyzer
 		{
 			// TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
 			// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-			context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+			context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
 		}
 
 		private static void AnalyzeSymbol(SymbolAnalysisContext context)
 		{
-			// TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
 			var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
 
-			// Find just those named type symbols with names containing lowercase letters.
-			if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-			{
-				// For all such symbols, produce a diagnostic.
-				var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+			context.ReportDiagnostic(Diagnostic.Create(Rule, namedTypeSymbol.Locations[0]));
 
-				context.ReportDiagnostic(diagnostic);
+			var attributes = namedTypeSymbol.GetAttributes();
+
+			foreach (var attribute in attributes)
+			{
+				if (attribute.AttributeClass.Name == "OnReceiveMessageAttribute")
+				{
+					var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], attribute.ConstructorArguments[0].Value.GetType().FullName);
+
+					context.ReportDiagnostic(diagnostic);
+				}
 			}
 		}
 	}
