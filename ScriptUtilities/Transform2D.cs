0bb455f0b03c9842a8b805827d230a6cd56ec2d8
Pixxl <oliverhalenius@yahoo.se>
2020-08-30 11:35:34 +0200

    add Transform2D

---
+++ ScriptUtilities/Transform2D.cs
@@ -0,0 +1,53 @@
+﻿using CrystalClear.ECS;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace CrystalClear.ScriptUtilities
+{
+	// TODO: Take parent objects into consideration for local and global positions (maybe a separate HierarhchyTransform2D even?)!
+	public class Transform2D : DataAttribute
+	{
+		public event Action TransformChanged;
+
+		private Vector2 position;
+		public Vector2 Position
+		{
+			get => position;
+			set
+			{
+				TransformChanged();
+				PositionChanged(value, position);
+				position = value;
+			}
+		}
+		public event Action<Vector2, Vector2> PositionChanged;
+
+		private float rotation;
+		public float Rotation
+		{
+			get => rotation;
+			set
+			{
+				TransformChanged();
+				RotationChanged(value, rotation);
+				rotation = value;
+			}
+		}
+		public event Action<float, float> RotationChanged;
+
+		private Vector2 scale;
+		public Vector2 Scale
+		{
+			get => scale;
+			set
+			{
+				TransformChanged();
+				ScaleChanged(value, scale);
+				scale = value;
+			}
+		}
+		public event Action<Vector2, Vector2> ScaleChanged;
+	}
+}
