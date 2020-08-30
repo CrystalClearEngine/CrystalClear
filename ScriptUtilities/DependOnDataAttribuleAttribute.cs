29dd45c80e39230a543f4d631abdaa81c75fceef
Pixxl <oliverhalenius@yahoo.se>
2020-08-30 02:46:26 +0200

    add DependOnDataAttributeAttribute

---
+++ ScriptUtilities/DependOnDataAttribuleAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrystalClear.ScriptUtilities
+{
+	public sealed class DependOnDataAttributeAttribute : Attribute
+	{
+		public Type DataAttributeType { get; }
+
+		public DependOnDataAttributeAttribute(Type dataAttributeType)
+		{
+			DataAttributeType = dataAttributeType;
+		}
+	}
+}
