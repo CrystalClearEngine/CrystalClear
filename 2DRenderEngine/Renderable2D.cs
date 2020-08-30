fbb8670c0a2064bc109a8ff88838677af85c8509
Pixxl <oliverhalenius@yahoo.se>
2020-08-30 11:35:52 +0200

    add Renderable2D and Renderable2DScript and RenderEngine2D

---
+++ 2DRenderEngine/Renderable2D.cs
@@ -0,0 +1,14 @@
+﻿using CrystalClear.ScriptUtilities;
+using SFML.Graphics;
+
+namespace CrystalClear.RenderEngine2D
+{
+	public class Renderable2D
+	{
+		public Sprite Sprite;
+
+		public Transform2D RenderTransform;
+
+		public uint RenderableId { get; internal set; }
+	}
+}
