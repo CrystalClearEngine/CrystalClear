567067e5a6b9494d34172ad3aed87ef9d5dd455d
Pixxl <oliverhalenius@yahoo.se>
2020-09-01 23:19:52 +0200

    add CreateNewWindow method

--- WindowingSystem/WindowingSystem.cs
+++ WindowingSystem/WindowingSystem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using SFML.System;
 using SFML.Window;
 
 namespace CrystalClear.WindowingSystem
@@ -7,6 +9,23 @@ namespace CrystalClear.WindowingSystem
 	// TODO: create OnWindowEventDispatch event?
 	public static class WindowingSystem
 	{
+		public static Window CreateNewWindow(string title, VideoMode videoMode, Vector2u? size = null, Vector2i? position = null)
+		{
+			var window = new Window(videoMode, title);
+			if (size.HasValue)
+			{
+				window.Size = size.Value;
+			}
+			if (position.HasValue)
+			{
+				window.Position = position.Value;
+			}
+			
+			windows.Add(window);
+
+			return window;
+		}
+		
 		// TODO: is this necessary?
 		public static Window MainWindow => Windows[0];
 
