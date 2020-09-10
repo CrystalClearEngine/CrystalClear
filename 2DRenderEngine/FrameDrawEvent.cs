6418b7446b28eb196c9140131152dea2ca9b7a19
Pixxl <oliverhalenius@yahoo.se>
2020-09-11 00:03:39 +0200

    add FrameDrawEvent

---
+++ 2DRenderEngine/FrameDrawEvent.cs
@@ -0,0 +1,22 @@
+﻿using CrystalClear.EventSystem;
+using CrystalClear.EventSystem.StandardEvents;
+
+namespace CrystalClear.RenderEngine2D
+{
+	/// <summary>
+	///     The frame draw event attribute.
+	/// </summary>
+	public sealed class OnFrameDrawAttribute : SubscribeToAttribute
+	{
+		public OnFrameDrawAttribute() : base(typeof(FrameDrawEvent))
+		{
+		}
+	}
+
+	/// <summary>
+	///     The frame draw event class.
+	/// </summary>
+	public class FrameDrawEvent : ScriptEvent<FrameDrawEvent>
+	{
+	}
+}
