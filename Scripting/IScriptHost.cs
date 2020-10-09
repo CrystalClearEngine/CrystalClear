625f01c8d655e60ffb2a2a39fb9def00d4e11f2c
Pixxl <oliverhalenius@yahoo.se>
2020-10-10 00:48:39 +0200

    WIP on scripting reimplementation

---
+++ Scripting/IScriptHost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrystalClear.Scripting
+{
+	// For anything that can hold scripts!
+	// TODO: make HierarchyObject use this.
+	interface IScriptHost
+	{
+	}
+
+	// TODO: I think IHierarchyScript should use this.
+	interface IScript<T>
+		where T : IScriptHost
+	{
+
+	}
+}
