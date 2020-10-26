9cd3907c9d0658af8a5dc80f81edcabbca82fc34
Pixxl <oliverhalenius@yahoo.se>
2020-10-26 19:44:12 +0100

    implement basic functional HierarchyViewer

--- UserInterface/ImGuiUserInterface.cs
+++ UserInterface/ImGuiUserInterface.cs
@@ -1,41 +1,44 @@
-﻿using ImGuiNET;
+﻿using CrystalClear.HierarchySystem;
+using CrystalClear.SerializationSystem.ImaginaryObjects;
+using EditorMain;
+using ImGuiNET;
+using System;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Threading;
 using Veldrid;
 using Veldrid.Sdl2;
 using Veldrid.StartupUtilities;
+using static CrystalClear.CrystalClearInformation;
+using static CrystalClear.EditorInformation;
+using static CrystalClear.Input;
 
 namespace CrystalClear.UserInterface
 {
 	public static class UserInterface
 	{
+		private static ImaginaryHierarchyObject rootHierarchyObject;
+		private static ImaginaryHierarchyObject currentSelectedHierarchyObject;
+		private static Assembly userAssembly;
+
 		public static void Main()
 		{
-			Sdl2Window window = WindowingSystem.WindowingSystem.CreateNewWindow("Veldrid test", width: 500, height: 500);
+			Sdl2Window window;
+			GraphicsDevice graphicsDevice;
+			ImGuiRenderer imguiRenderer;
+			CommandList cl;
 
-			GraphicsDeviceOptions gdOptions = new GraphicsDeviceOptions(false,
-															   null,
-															   false,
-															   ResourceBindingModel.Improved,
-															   true,
-															   true,
-															   true);
-
-			var graphicsDevice = VeldridStartup.CreateGraphicsDevice(window, gdOptions);
-
-			ImGuiRenderer imguiRenderer = new ImGuiRenderer(
-				graphicsDevice, graphicsDevice.MainSwapchain.Framebuffer.OutputDescription,
-				(int)graphicsDevice.MainSwapchain.Framebuffer.Width, (int)graphicsDevice.MainSwapchain.Framebuffer.Height);
-
-			var cl = graphicsDevice.ResourceFactory.CreateCommandList();
+			SetUpRenderer(out window, out graphicsDevice, out imguiRenderer, out cl);
 
 			window.Resized += () => imguiRenderer.WindowResized(window.Width, window.Height);
 			window.Resized += () => graphicsDevice.ResizeMainWindow((uint)window.Width, (uint)window.Height);
 
-			ImGui.StyleColorsClassic();
+			ChooseProject();
+
+			SetUpEditor();
 
 			int optimalFrameTimeMS = 16;
-			bool showMore = false;
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
 			while (window.Exists)
@@ -46,11 +49,7 @@ namespace CrystalClear.UserInterface
 				if (!window.Exists) { break; }
 				imguiRenderer.Update((float)stopwatch.Elapsed.TotalSeconds, input); // Compute actual value for deltaSeconds.
 
-				// Draw stuff
-				ImGui.Begin("Hierarchy");
-				{
-					
-				} ImGui.End();
+				UI(ref rootHierarchyObject, ref currentSelectedHierarchyObject, userAssembly);
 
 				cl.Begin();
 				cl.SetFramebuffer(graphicsDevice.MainSwapchain.Framebuffer);
@@ -61,6 +60,114 @@ namespace CrystalClear.UserInterface
 				graphicsDevice.SwapBuffers(graphicsDevice.MainSwapchain);
 
 				stopwatch.Restart();
+			}
+		}
+
+		private static void ChooseProject()
+		{
+			Output.Log("Please open or create a new project:");
+			ProjectSelection:
+			switch (Console.ReadLine())
+			{
+				case "new":
+					ProjectInfo.NewProject(AskQuestion("Pick a path for the new project"),
+						AskQuestion("Pick a name for the new project"));
+					break;
+
+				case "open":
+					try
+					{
+						ProjectInfo.OpenProject(AskQuestion("Enter the path of the project"));
+					}
+					catch (ArgumentException)
+					{
+						goto ProjectSelection;
+					}
+
+					break;
+
+				default:
+					Output.ErrorLog("command error: unknown command");
+					goto ProjectSelection;
+			}
+		}
+
+		private static void SetUpRenderer(out Sdl2Window window, out GraphicsDevice graphicsDevice, out ImGuiRenderer imguiRenderer, out CommandList cl)
+		{
+			window = WindowingSystem.WindowingSystem.CreateNewWindow("Veldrid test", width: 500, height: 500);
+			GraphicsDeviceOptions gdOptions = new GraphicsDeviceOptions(false,
+															   null,
+															   false,
+															   ResourceBindingModel.Improved,
+															   true,
+															   true,
+															   true);
+
+			graphicsDevice = VeldridStartup.CreateGraphicsDevice(window, gdOptions);
+			imguiRenderer = new ImGuiRenderer(
+				graphicsDevice, graphicsDevice.MainSwapchain.Framebuffer.OutputDescription,
+				(int)graphicsDevice.MainSwapchain.Framebuffer.Width, (int)graphicsDevice.MainSwapchain.Framebuffer.Height);
+			cl = graphicsDevice.ResourceFactory.CreateCommandList();
+
+			ImGui.StyleColorsClassic();
+		}
+
+		private static void SetUpEditor()
+		{
+			{
+				FileInfo[] files = CurrentProject.ScriptsDirectory.GetFiles("*.cs");
+
+				CodeFilePaths = new string[files.Length];
+
+				for (var i = 0; i < files.Length; i++)
+					CodeFilePaths[i] = files[i].FullName;
+			}
+
+			// Compile the code.
+			userAssembly = MainClass.Compile();
+
+			MainClass.Analyze(userAssembly);
+
+			var fileSystemWatcher = new FileSystemWatcher(CurrentProject.ScriptsDirectory.FullName, "*.cs");
+			fileSystemWatcher.Changed += (_, _1) =>
+			{
+				Output.Log("Code change detected, recompiling.");
+				CodeFilePaths = Directory.GetFiles(CurrentProject.ScriptsDirectory.FullName, "*.cs");
+				// TODO: something to wait until the file is ready.
+				Thread.Sleep(100);
+				MainClass.Compile();
+				MainClass.Analyze(userAssembly);
+			};
+			fileSystemWatcher.EnableRaisingEvents = true;
+
+			rootHierarchyObject =
+				new ImaginaryHierarchyObject(null, new ImaginaryConstructableObject(typeof(HierarchyRoot)));
+				ImaginaryHierarchyObject currentSelectedHierarchyObject = rootHierarchyObject;
+
+			rootHierarchyObject.LocalHierarchy.Add("Child1", new ImaginaryHierarchyObject(rootHierarchyObject, new ImaginaryConstructableObject(typeof(HierarchyObject))));
+			rootHierarchyObject.LocalHierarchy.Add("Child2", new ImaginaryHierarchyObject(rootHierarchyObject, new ImaginaryConstructableObject(typeof(HierarchyObject))));
+			rootHierarchyObject.LocalHierarchy.Add("Child3", new ImaginaryHierarchyObject(rootHierarchyObject, new ImaginaryConstructableObject(typeof(HierarchyObject))));
+
+			rootHierarchyObject.LocalHierarchy["Child1"].LocalHierarchy.Add("ChildChild1", new ImaginaryHierarchyObject(rootHierarchyObject.LocalHierarchy["Child1"], new ImaginaryConstructableObject(typeof(HierarchyObject))));
+		}
+
+		private static void UI(ref ImaginaryHierarchyObject rootHierarchyObject, ref ImaginaryHierarchyObject currentSelectedHierarchyObject, Assembly userGeneratedCode)
+		{
+			ImGui.Begin("Hierarchy");
+			{
+				CreateTreeForHierarchyObject(rootHierarchyObject);
+			} ImGui.End();
+		}
+
+		private static void CreateTreeForHierarchyObject(ImaginaryHierarchyObject hierarchyObject)
+		{
+			if (ImGui.TreeNode(hierarchyObject.Name))
+			{
+				foreach (var hierarchyObjectChild in hierarchyObject.LocalHierarchy)
+				{
+					CreateTreeForHierarchyObject(hierarchyObject.LocalHierarchy[hierarchyObjectChild.Key]);
+				}
+				ImGui.TreePop();
 			}
 		}
 	}
