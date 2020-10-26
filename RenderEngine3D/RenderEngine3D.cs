4656c8ff7a8607c2cb0b66ae01fa8aea03dac2f6
Pixxl <oliverhalenius@yahoo.se>
2020-10-26 12:46:30 +0100

    add new RenderEngine3D project

---
+++ RenderEngine3D/RenderEngine3D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CrystalClear.WindowingSystem;
+using SFML.Window;
+using Veldrid;
+
+namespace RenderEngine3D
+{
+	public static class RenderEngine3D
+	{
+		public static void Main()
+		{
+			Window window = WindowingSystem.CreateNewWindow("Veldrid test", VideoMode.DesktopMode);
+
+			GraphicsDeviceOptions gdOptions = new GraphicsDeviceOptions(false,
+															   null,
+															   false,
+															   ResourceBindingModel.Improved,
+															   true,
+															   true,
+															   true);
+		}
+	}
+}
