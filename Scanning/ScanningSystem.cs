4c879c0be42640affe32e9623685ada1f2f21edb
Pixxl <oliverhalenius@yahoo.se>
2020-11-01 22:49:41 +0100

    start on new ScanningSystem

---
+++ Scanning/ScanningSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CrystalClear.ScanningSystem
+{
+	public static class ScanningSystem
+	{
+		public static IEnumerable<Type> FindTypesWithAttribute(Type[] types, Type attributeType, bool checkInheritance)
+		{
+			foreach (var type in types)
+			{
+				if (type.GetCustomAttributes(attributeType, checkInheritance).Any((obj) => obj.GetType() == attributeType))
+				{
+					yield return type;
+				}
+			}
+		}
+
+		public static IEnumerable<Type> FindTypesWithAttribute<TAttribute>(Type[] types, bool checkInheritance)
+		{
+			foreach (var type in types)
+			{
+				if (type.GetCustomAttributes(typeof(TAttribute), checkInheritance).Any((obj) => obj.GetType() is TAttribute))
+				{
+					yield return type;
+				}
+			}
+		}
+
+		public static Type[] FindSubclasses()
+		{
+
+		}
+	}
+}
