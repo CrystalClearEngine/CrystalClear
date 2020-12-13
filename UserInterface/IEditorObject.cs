970abad2cd8a6a36285d8a75da13f92a9e2cbd7f
Pixxl <oliverhalenius@yahoo.se>
2020-12-13 19:59:05 +0100

    add IEditorObject interface

---
+++ UserInterface/IEditorObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrystalClear.UserInterface
+{
+	public interface IEditorObject
+	{
+		public void ModifierUI()
+		{
+			EditorGUI.Text("No custom Modifier UI has been made for this object.");
+		}
+	}
+}
